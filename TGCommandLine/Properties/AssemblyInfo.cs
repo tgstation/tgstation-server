e9c39834d4bc440f60d9a248f613572aecaf68c8
Cyberboss <Cyberboss@users.noreply.github.com>
2017-06-23 13:34:47 -0400

    Version bump to 3.0.85.2 [TGSDeploy]

--- TGCommandLine/Properties/AssemblyInfo.cs
+++ TGCommandLine/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.85.1")]
-[assembly: AssemblyFileVersion("3.0.85.1")]
+[assembly: AssemblyVersion("3.0.85.2")]
+[assembly: AssemblyFileVersion("3.0.85.2")]
