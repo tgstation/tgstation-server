67b9efff65bb08ab144464ac5cd993d094ab39cd
Cyberboss <Cyberboss@users.noreply.github.com>
2017-07-04 15:22:28 -0400

    Unifies the Version

--- TGServerService/Properties/AssemblyInfo.cs
+++ TGServerService/Properties/AssemblyInfo.cs
@@ -14,16 +14,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f32eda25-0855-411c-af5e-f0d042917e2d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.87.0")]
-[assembly: AssemblyFileVersion("3.0.87.0")]
