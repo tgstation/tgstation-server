fd8c154d4193f380a738f0a51bf16520576561cf
Cyberboss <Cyberboss@users.noreply.github.com>
2017-07-05 14:24:06 -0400

    Fix chat settings getting reset

--- TGServerService/Properties/Settings.Designer.cs
+++ TGServerService/Properties/Settings.Designer.cs
@@ -217,7 +217,7 @@ namespace TGServerService.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1")]
         public int SettingsVersion {
             get {
                 return ((int)(this["SettingsVersion"]));
