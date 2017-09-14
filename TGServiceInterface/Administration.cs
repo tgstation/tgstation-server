e82ee3a9c42f4a2e813c1571ee84facc943000f9
Cyberboss <Cyberboss@users.noreply.github.com>
2017-09-14 14:41:00 -0400

    Splits up Server.cs

---
+++ TGServiceInterface/Administration.cs
@@ -0,0 +1,26 @@
+﻿using System.ServiceModel;
+
+namespace TGServiceInterface
+{
+	/// <summary>
+	/// Manage the group that is used to access the service, can only be used by an administrator
+	/// </summary>
+	[ServiceContract]
+	public interface ITGAdministration
+	{
+		/// <summary>
+		/// Returns the name of the windows group allowed to use the service other than administrator
+		/// </summary>
+		/// <returns>The name of the windows group allowed to use the service other than administrator, "ADMIN" if it's unset, null on failure</returns>
+		[OperationContract]
+		string GetCurrentAuthorizedGroup();
+
+		/// <summary>
+		/// Searches the windows machine for the group named <paramref name="groupName"/>, sets it as the authorized group if it's found
+		/// </summary>
+		/// <param name="groupName">The name of the group to search for or null to clear the setting</param>
+		/// <returns>The full name of the group that is now authorized on success, null on failure, "ADMIN" on clearing</returns>
+		[OperationContract]
+		string SetAuthorizedGroup(string groupName);
+	}
+}
---
+++ TGServiceInterface/Service.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace TGServiceInterface
+{
+	/// <summary>
+	/// Interface for managing the service
+	/// </summary>
+	[ServiceContract]
+	public interface ITGSService
+	{
+
+		/// <summary>
+		/// Next stop of the service will not close DD and sets a flag for it to reattach once it restarts
+		/// </summary>
+		[OperationContract]
+		void PrepareForUpdate();
+
+		[OperationContract]
+		[Obsolete("StopForUpdate is deprecated, please use PrepareForUpdate instead")]
+		void StopForUpdate();
+
+		/// <summary>
+		/// Retrieve's the service's version
+		/// </summary>
+		/// <returns>The service's version</returns>
+		[OperationContract]
+		string Version();
+	}
+}
