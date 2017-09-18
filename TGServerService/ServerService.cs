52afcf420af5b2a6a8ad9f35b677e8e79b4287cc
Cyberboss <Cyberboss@users.noreply.github.com>
2017-09-18 16:09:10 -0400

    Removes some uneeded includes

--- TGServerService/ServerService.cs
+++ TGServerService/ServerService.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Security.Cryptography.X509Certificates;
 using System.ServiceModel;
 using System.ServiceProcess;
 using TGServiceInterface;
