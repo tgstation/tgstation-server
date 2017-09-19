4a056cb37e8de390c24ee4d60c83bb157dd22975
Cyberboss <Cyberboss@users.noreply.github.com>
2017-09-19 12:31:29 -0400

    Rapid fire commiting

--- build_byond.sh
+++ build_byond.sh
@@ -4,11 +4,20 @@ set -e
 retval=1
 source $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/byondsetup
 
-DreamMaker $DMEName 2>&1 | tee result.log
-retval=$?
-if ! grep '\- 0 errors, 0 warnings' result.log
+env
+file $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/DreamMaker
+env
+if hash $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/DreamMaker 2>/dev/null
 then
-	retval=1 #hard fail, due to warnings or errors
+	env
+	$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/DreamMaker $DMEName 2>&1 | tee result.log
+	retval=$?
+	if ! grep '\- 0 errors, 0 warnings' result.log
+	then
+		retval=1 #hard fail, due to warnings or errors
+	fi
+else
+	echo "Couldn't find the DreamMaker executable, aborting."
+	retval=2
 fi
-
 exit $retval
