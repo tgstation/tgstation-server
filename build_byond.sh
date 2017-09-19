249a792eda4f3f42ce2b86c4553de58e66613461
Cyberboss <Cyberboss@users.noreply.github.com>
2017-09-19 12:37:04 -0400

    Throw on the libs from tgstation cause why not?

--- build_byond.sh
+++ build_byond.sh
@@ -4,13 +4,9 @@ set -e
 retval=1
 source $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/byondsetup
 
-env
-file $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/DreamMaker
-env
-if hash $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/DreamMaker 2>/dev/null
+if hash DreamMaker 2>/dev/null
 then
-	env
-	$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/DreamMaker $DMEName 2>&1 | tee result.log
+	DreamMaker $DMEName 2>&1 | tee result.log
 	retval=$?
 	if ! grep '\- 0 errors, 0 warnings' result.log
 	then
