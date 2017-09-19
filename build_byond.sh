2220a7cddda2027045e48b85f7f994aeb68a862a
Cyberboss <Cyberboss@users.noreply.github.com>
2017-09-19 12:27:52 -0400

    Gonna be a madlad for a second

--- build_byond.sh
+++ build_byond.sh
@@ -4,19 +4,11 @@ set -e
 retval=1
 source $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/byondsetup
 
-ls $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin
-file $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/DreamMaker
-
-if hash $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/DreamMaker 2>/dev/null
+DreamMaker $DMEName 2>&1 | tee result.log
+retval=$?
+if ! grep '\- 0 errors, 0 warnings' result.log
 then
-	$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/DreamMaker $DMEName 2>&1 | tee result.log
-	retval=$?
-	if ! grep '\- 0 errors, 0 warnings' result.log
-	then
-		retval=1 #hard fail, due to warnings or errors
-	fi
-else
-	echo "Couldn't find the DreamMaker executable, aborting."
-	retval=2
+	retval=1 #hard fail, due to warnings or errors
 fi
+
 exit $retval
