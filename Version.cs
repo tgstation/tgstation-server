3bb94f75176775ed88ad93f8e532f04e95c2cc0d
Cyberboss <Cyberboss@users.noreply.github.com>
2017-09-24 14:19:20 -0400

    Version bump to 3.1.0.4

--- Version.cs
+++ Version.cs
@@ -12,6 +12,6 @@ using System.Reflection;
 // [assembly: AssemblyVersion("1.0.*")]
 
 //It's impossible to make these a define, don't say I didn't warn you 
-[assembly: AssemblyVersion("3.1.0.3")]
-[assembly: AssemblyFileVersion("3.1.0.3")]
-[assembly: AssemblyInformationalVersion("3.1.0.3")]
+[assembly: AssemblyVersion("3.1.0.4")]
+[assembly: AssemblyFileVersion("3.1.0.4")]
+[assembly: AssemblyInformationalVersion("3.1.0.4")]
