c984386358e968eb6d4da5f8a720384780730505
Cyberboss <Cyberboss@users.noreply.github.com>
2017-09-24 09:39:45 -0400

    Fixes deploy script

--- TGS3Release.ps1
+++ TGS3Release.ps1
@@ -4,7 +4,6 @@ $src = "$bf\TGInstallerWrapper\bin\Release"
 Remove-Item "$src\Microsoft.Deployment.WindowsInstaller.xml"
 Remove-Item "$src\TG Station Server Installer.exe.config"
 Remove-Item "$src\TG Station Server Installer.pdb"
-Remove-Item "$src\TGServiceInterface.pdb"
 
 $destination = "$bf\TGS3-Server.zip"
 
@@ -20,7 +19,7 @@ $src2 = $Env:APPVEYOR_BUILD_FOLDER + "\ClientApps"
 [system.io.directory]::CreateDirectory($src2)
 Copy-Item "$bf\TGCommandLine\bin\Release\TGCommandLine.exe" "$src2\TGCommandLine.exe"
 Copy-Item "$bf\TGControlPanel\bin\Release\TGControlPanel.exe" "$src2\TGControlPanel.exe"
-Copy-Item "$bf\TGServiceInterface\bin\Release\TGServiceInterface.dll" "$src2\TGServiceInterface.dll"
+Copy-Item "$bf\TGServiceInterface\bin\x86\Release\TGServiceInterface.dll" "$src2\TGServiceInterface.dll"
 
 $dest2 = "$bf\TGS3-Client.zip"
 
