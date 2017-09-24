dc0bd94e1f9ead8b3fbd093c4a76e32deed69002
Cyberboss <Cyberboss@users.noreply.github.com>
2017-09-24 08:38:05 -0400

    Tabs

--- TGInstallerWrapper/Main.cs
+++ TGInstallerWrapper/Main.cs
@@ -114,7 +114,7 @@ namespace TGInstallerWrapper
 					try
 					{
 						Server.GetComponent<ITGSService>().PrepareForUpdate();
-                        Thread.Sleep(3000); //chat messages
+						Thread.Sleep(3000); //chat messages
 					}
 					catch
 					{
