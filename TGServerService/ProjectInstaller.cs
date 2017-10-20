3fd79371d9dc8e78fb82e823bb603c7e4e457f4d
Cyberboss <Cyberboss@users.noreply.github.com>
2017-10-20 19:20:29 -0400

    Document the ProjectInstaller

--- TGServerService/ProjectInstaller.cs
+++ TGServerService/ProjectInstaller.cs
@@ -3,9 +3,15 @@ using System.Configuration.Install;
 
 namespace TGServerService
 {
+	/// <summary>
+	/// This tells the .msi there is a Windows <see cref="System.ServiceProcess.ServiceBase"/> in this <see cref="System.Reflection.Assembly"/> that needs installation
+	/// </summary>
 	[RunInstaller(true)]
 	partial class ProjectInstaller : Installer
 	{
+		/// <summary>
+		/// Construct a <see cref="ProjectInstaller"/>
+		/// </summary>
 		public ProjectInstaller()
 		{
 			InitializeComponent();
