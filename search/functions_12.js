0666e549c932e45ad863006ae73b0eb02915bb40
Appveyor CI <ci@appveyor.com>
2017-11-02 07:39:50 -0700

    Deploy code docs to GitHub Pages for Appveyor build 376
    Commit: 3dbd5a1d8862080ff98a51532523b8646884f604

--- search/functions_12.js
+++ search/functions_12.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['tellservicewerecomingforthem',['TellServiceWereComingForThem',['../class_t_g_installer_wrapper_1_1_main.html#ae06c0eea3d92868ed601fc39246cc2d7',1,'TGInstallerWrapper::Main']]],
-  ['testmergebutton_5fclick',['TestMergeButton_Click',['../class_t_g_control_panel_1_1_control_panel.html#aac9296134e532fab8d4879ca9b1891bf',1,'TGControlPanel.ControlPanel.TestMergeButton_Click(object sender, EventArgs e)'],['../class_t_g_control_panel_1_1_control_panel.html#a5fd257a72a8eaacd28e6f8454ddeb389',1,'TGControlPanel.ControlPanel.TestmergeButton_Click(object sender, System.EventArgs e)']]],
+  ['testmergebutton_5fclick',['TestmergeButton_Click',['../class_t_g_control_panel_1_1_control_panel.html#a5fd257a72a8eaacd28e6f8454ddeb389',1,'TGControlPanel.ControlPanel.TestmergeButton_Click(object sender, System.EventArgs e)'],['../class_t_g_control_panel_1_1_control_panel.html#aac9296134e532fab8d4879ca9b1891bf',1,'TGControlPanel.ControlPanel.TestMergeButton_Click(object sender, EventArgs e)']]],
   ['testmergecommand',['TestmergeCommand',['../class_t_g_command_line_1_1_testmerge_command.html#a8dbbc7f25b95f9da87feac96dbf44aa6',1,'TGCommandLine::TestmergeCommand']]],
   ['textprompt',['TextPrompt',['../class_t_g_control_panel_1_1_program.html#a6b586d2d467f8a2cf69d01ab03a2eac6',1,'TGControlPanel::Program']]],
   ['tgsjsonupdate_5fclick',['TGSJsonUpdate_Click',['../class_t_g_control_panel_1_1_control_panel.html#a549ec09325e6d14d8448448634a13536',1,'TGControlPanel::ControlPanel']]]
--- search/properties_6.js
+++ search/properties_6.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['instance',['Instance',['../class_t_g_server_service_1_1_chat_commands_1_1_chat_command.html#ad88aca64f613d0f1e7d2ccc662e368b1',1,'TGServerService::ChatCommands::ChatCommand']]],
   ['isadmin',['IsAdmin',['../class_t_g_server_service_1_1_chat_commands_1_1_command_info.html#a9a0a92e23e1e56bffd52edb424b07cee',1,'TGServerService::ChatCommands::CommandInfo']]],
-  ['isadminchannel',['IsAdminChannel',['../class_t_g_server_service_1_1_chat_commands_1_1_command_info.html#a66486d6603e710ee058f55232f5a07f3',1,'TGServerService::ChatCommands::CommandInfo']]]
+  ['isadminchannel',['IsAdminChannel',['../class_t_g_server_service_1_1_chat_commands_1_1_command_info.html#a66486d6603e710ee058f55232f5a07f3',1,'TGServerService::ChatCommands::CommandInfo']]],
+  ['isremoteconnection',['IsRemoteConnection',['../class_t_g_service_interface_1_1_interface.html#a6191fad5a87576296b7f73d307934a3f',1,'TGServiceInterface::Interface']]]
 ];
