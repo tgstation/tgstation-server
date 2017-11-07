20e0a78bf44d2342c96e2e1ddf7e55a6bcf5e448
Cyberboss <Cyberboss@users.noreply.github.com>
2017-11-07 10:19:42 -0500

    Coverage fixes, do it the way CyberEngineMkIII does it

---
+++ Tools/GenCodeCovXML.ps1
@@ -0,0 +1,15 @@
+$coverageFilePath = Resolve-Path -path "TestResults\*\*.coverage"
+ 
+$coverageFilePath = $coverageFilePath.ToString()
+
+Write-Host "Running CodeCoverage.exe..."
+&"C:\Program Files (x86)\Microsoft Visual Studio\Community\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" analyze /output:coverage.coveragexml "$coverageFilePath"
+
+rm -r TestResults
+
+Write-Host "Downloading PathCapitalizationCorrector v0.1.4..."
+appveyor DownloadFile https://github.com/Cyberboss/PathCapitalizationCorrector/releases/download/0.1.4/PathCapitalizationCorrector.exe
+
+Write-Host "Fixing Window's terrible case ignorance..."
+&"./PathCapitalizationCorrector.exe" coverage.coveragexml
+codecov -f coverage.coveragexml
