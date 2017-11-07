aced691b093bf17093ac605430176a26b880bae8
Cyberboss <Cyberboss@users.noreply.github.com>
2017-11-07 11:10:12 -0500

    Fix VS path

--- Tools/GenCodeCovXML.ps1
+++ Tools/GenCodeCovXML.ps1
@@ -3,7 +3,7 @@ $coverageFilePath = Resolve-Path -path "TestResults\*\*.coverage"
 $coverageFilePath = $coverageFilePath.ToString()
 
 Write-Host "Running CodeCoverage.exe..."
-&"C:\Program Files (x86)\Microsoft Visual Studio\Community\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" analyze /output:coverage.coveragexml "$coverageFilePath"
+&"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" analyze /output:coverage.coveragexml "$coverageFilePath"
 
 rm -r TestResults
 
