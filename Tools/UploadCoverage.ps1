05728babcff0a6efa5f5715df84b40eda193aefc
Cyberboss <Cyberboss@users.noreply.github.com>
2017-11-07 12:03:27 -0500

    Remove PathCapitalizationCorrector

--- Tools/UploadCoverage.ps1
+++ Tools/UploadCoverage.ps1
@@ -7,9 +7,4 @@ Write-Host "Running CodeCoverage.exe..."
 
 rm -r TestResults
 
-Write-Host "Downloading PathCapitalizationCorrector v0.1.4..."
-appveyor DownloadFile https://github.com/Cyberboss/PathCapitalizationCorrector/releases/download/0.1.4/PathCapitalizationCorrector.exe
-
-Write-Host "Fixing Window's terrible case ignorance..."
-&"./PathCapitalizationCorrector.exe" coverage.coveragexml
 codecov -f coverage.coveragexml
