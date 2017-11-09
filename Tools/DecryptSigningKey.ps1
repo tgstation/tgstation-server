24cbb686da3bcc400d3c683cacf669bb2c441ece
Cyberboss <Cyberboss@users.noreply.github.com>
2017-11-09 16:46:05 -0500

    Modify AssemblyInfo.global.cs to enable signing

--- Tools/DecryptSigningKey.ps1
+++ Tools/DecryptSigningKey.ps1
@@ -50,3 +50,7 @@ Remove-Item "$bf/Tools/TGStationServer3.snk"
 
 #Replace bad references with our PKTok 1521fb3fe76ed910
 ReplaceTextInFile 'PublicKeyToken=null' 'PublicKeyToken=1521fb3fe76ed910' "$bf/TGServerService/TGServerService.csproj"
+
+#OK all good, modify AssemblyInfo.global.cs to enable strong naming
+ReplaceTextInFile 'TGServiceTests' 'TGServiceTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f10487511f8056df7ead40f8f3bb0a7a4890d1bafdbf3d2cc0092655849223733672039671c3855e653c950b68b6a9dd04fbe0784f0c6e213c66be8afa4cc37afad52a05744c1305bf1d1c7c2702b4f64036c5b96045a7ccdc421847eec17203ad8188b5a34a5d5cd3c845a071ebf72fff1236410cce8d51616a49f6e53ba0b9' "$bf/AssemblyInfo.global.cs"
+Add-Content "$bf/AssemblyInfo.global.cs" "#if !NO_STRONG_NAME`n[assembly: AssemblyKeyName(`"TGStationServer3`")]`n#endif"
