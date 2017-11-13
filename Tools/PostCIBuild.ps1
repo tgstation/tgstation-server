84906eaa2e1223032b905e5580632b3ed9befb9a
Cyberboss <Cyberboss@users.noreply.github.com>
2017-11-13 00:27:55 -0500

    Add Server Console artifact to CI

--- Tools/PostCIBuild.ps1
+++ Tools/PostCIBuild.ps1
@@ -11,6 +11,7 @@ function CodeSign
 if (Test-Path env:snk_passphrase)
 {
 	CodeSign "$bf/TGS.Tests/bin/Release/TGS.Tests.dll"
+	CodeSign "$bf/TGS.Server.Console/bin/Release/TGS.Server.Console.exe"
 	CodeSign "$bf/TGS.Installer.UI/bin/Release/TG Station Server Installer.exe"
 	$env:snk_passphrase = ""
 }
@@ -36,7 +37,23 @@ Copy-Item "$bf\TGS.Interface\bin\Release\TGServiceInterface.dll" "$src2\TGServic
 $dest2 = "$bf\TGS3-Client-v$version.zip"
 
 [io.compression.zipfile]::CreateFromDirectory($src2, $dest2) 
+
+$src3 = "$bf\ServerConsole"
+[system.io.directory]::CreateDirectory($src3)
+Copy-Item "$bf\TGS.CommandLine\bin\Release\TGCommandLine.exe" "$src3\TGCommandLine.exe"
+Copy-Item "$bf\TGS.ControlPanel\bin\Release\TGControlPanel.exe" "$src3\TGControlPanel.exe"
+Copy-Item "$bf\TGS.ControlPanel\bin\Release\Octokit.dll" "$src3\Octokit.dll"
+Copy-Item "$bf\TGS.Interface\bin\Release\TGServiceInterface.dll" "$src3\TGServiceInterface.dll"
+Copy-Item "$bf\TGS.Server\bin\Release\TGS.Server.dll" "$src3\TGS.Server.dll"
+Copy-Item "$bf\TGS.Server.Console\bin\Release\TGS.Server.Console.exe" "$src3\TGS.Server.Console.exe"
+
+$dest3 = "$bf\TGS3-ServerConsole-v$version.zip"
+
+[io.compression.zipfile]::CreateFromDirectory($src3, $dest3) 
+
 $destination_md5sha2 = "$bf\MD5-SHA1-Client-v$version.txt"
+$destination_md5sha3 = "$bf\MD5-SHA1-ServerConsole-v$version.txt"
 
 & fciv -both $destination > $destination_md5sha
 & fciv -both $dest2 > $destination_md5sha2
+& fciv -both $dest3 > $destination_md5sha3
