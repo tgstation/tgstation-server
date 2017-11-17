a676319b4f0825b5eb7cfb287816cd1524418f1a
Jordan Brown <Cyberboss@users.noreply.github.com>
2017-11-17 15:10:12 -0500

    Fixes ReattachAPIVersion not being set

--- TGS.Server/Instance/DreamDaemon.cs
+++ TGS.Server/Instance/DreamDaemon.cs
@@ -407,6 +407,7 @@ namespace TGS.Server
 					{
 						Config.ReattachProcessID = Proc.Id;
 						Config.ReattachPort = currentPort;
+						Config.ReattachAPIVersion = GameAPIVersion != null ? GameAPIVersion.ToString() : null;
 						Config.ReattachCommsKey = serviceCommsKey;
 						lock (restartLock)
 						{
