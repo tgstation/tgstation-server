f87a438b40d2f9395ebae7d0d7102ec56489e67f
Cyberboss <Cyberboss@users.noreply.github.com>
2017-11-18 23:45:10 -0500

    Docs for PopulateCommandInfoEventArgs

--- TGS.Server/Components/PopulateCommandInfoEventArgs.cs
+++ TGS.Server/Components/PopulateCommandInfoEventArgs.cs
@@ -1,14 +1,26 @@
 ﻿using System;
-using TGS.Server.ChatCommands;
 
-namespace TGS.Server.Components
+namespace TGS.Server.ChatCommands
 {
+	/// <summary>
+	/// Used for populating a <see cref="ChatCommands.CommandInfo"/> structure
+	/// </summary>
 	sealed class PopulateCommandInfoEventArgs : EventArgs
 	{
+		/// <summary>
+		/// The <see cref="ChatCommands.CommandInfo"/> to populate
+		/// </summary>
 		public CommandInfo CommandInfo{ get { return _commandInfo; } }
 
+		/// <summary>
+		/// Backing variable for <see cref="CommandInfo"/>
+		/// </summary>
 		readonly CommandInfo _commandInfo;
 
+		/// <summary>
+		/// Construct a <see cref="PopulateCommandInfoEventArgs"/>
+		/// </summary>
+		/// <param name="ci">The valuse of <see cref="_commandInfo"/></param>
 		public PopulateCommandInfoEventArgs(CommandInfo ci)
 		{
 			_commandInfo = ci;
