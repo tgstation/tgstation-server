94b11b4ac13796218bc9a600c2845a53915b68f3
Cyberboss <Cyberboss@users.noreply.github.com>
2017-11-28 15:22:24 -0500

    Document new custom authentication scheme

--- TGS.Interface/AuthenticationHeaderApplicator.cs
+++ TGS.Interface/AuthenticationHeaderApplicator.cs
@@ -8,15 +8,30 @@ using System.ServiceModel.Description;
 
 namespace TGS.Interface
 {
+	/// <summary>
+	/// Used to attach windows credential headers to SOAP messages
+	/// </summary>
 	sealed class AuthenticationHeaderApplicator : IEndpointBehavior, IClientMessageInspector
 	{
+		/// <summary>
+		/// The credentials to attach
+		/// </summary>
 		readonly RemoteLoginInfo remoteLoginInfo;
-			
+		
+		/// <summary>
+		/// Construct a <see cref="AuthenticationHeaderApplicator"/>
+		/// </summary>
+		/// <param name="loginInfo">The <see cref="RemoteLoginInfo"/> to use</param>
 		public AuthenticationHeaderApplicator(RemoteLoginInfo loginInfo)
 		{
 			remoteLoginInfo = loginInfo;
 		}
 
+		/// <summary>
+		/// Add <see langword="this"/> to the message inspectors for the channel
+		/// </summary>
+		/// <param name="endpoint">The <see cref="ServiceEndpoint"/></param>
+		/// <param name="clientRuntime">The <see cref="ClientRuntime"/></param>
 		public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
 		{
 			var t = Type.GetType("Mono.Runtime");
@@ -34,6 +49,12 @@ namespace TGS.Interface
 			inspectors.Add(this);
 		}
 
+		/// <summary>
+		/// Attach <see cref="RemoteLoginInfo.Username"/> and <see cref="RemoteLoginInfo.Password"/> to the <paramref name="request"/>
+		/// </summary>
+		/// <param name="request">The outgoing request</param>
+		/// <param name="channel">The <see cref="IClientChannel"/></param>
+		/// <returns></returns>
 		public object BeforeSendRequest(ref Message request, IClientChannel channel)
 		{
 			request.Headers.Add(MessageHeader.CreateHeader("Username", "http://tempuri.org", remoteLoginInfo.Username));
@@ -41,21 +62,33 @@ namespace TGS.Interface
 			return null;
 		}
 
+		/// <summary>
+		/// Unused implementation of <see cref="IEndpointBehavior"/>
+		/// </summary>
 		public void AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters)
 		{
 			//intentionally left blank
 		}
 
+		/// <summary>
+		/// Unused implementation of <see cref="IClientMessageInspector"/>
+		/// </summary>
 		public void AfterReceiveReply(ref Message reply, object correlationState)
 		{
 			//intentionally left blank
 		}
 
+		/// <summary>
+		/// Unused implementation of <see cref="IEndpointBehavior"/>
+		/// </summary>
 		public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
 		{
 			//intentionally left blank
 		}
 
+		/// <summary>
+		/// Unused implementation of <see cref="IEndpointBehavior"/>
+		/// </summary>
 		public void Validate(ServiceEndpoint endpoint)
 		{
 			//intentionally left blank
--- TGS.Server/Security/AuthenticationHeaderDecoder.cs
+++ TGS.Server/Security/AuthenticationHeaderDecoder.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Collections.Generic;
-using System.IdentityModel.Claims;
 using System.IdentityModel.Policy;
 using System.Runtime.InteropServices;
+using System.Security.Principal;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
 
@@ -11,15 +11,24 @@ namespace TGS.Server.Security
 {
 	sealed class AuthenticationHeaderDecoder : ServiceAuthenticationManager
 	{
+		/// <summary>
+		/// See https://msdn.microsoft.com/en-us/library/windows/desktop/aa378184(v=vs.85).aspx
+		/// </summary>
 		[DllImport("advapi32.dll", SetLastError = true)]
 		static extern bool LogonUser(string lpszUsername, string lpszDomain, string lpszPassword, int dwLogonType, int dwLogonProvider, out IntPtr phToken);
+		/// <summary>
+		/// See https://msdn.microsoft.com/en-us/library/windows/desktop/ms724211(v=vs.85).aspx
+		/// </summary>
 		[DllImport("kernel32.dll", SetLastError = true)]
 		static extern bool CloseHandle(IntPtr handle);
 
-		public ClaimSet Issuer => throw new NotImplementedException();
-
-		public string Id => throw new NotImplementedException();
-
+		/// <summary>
+		/// Authenticates a given <see cref="Message"/>
+		/// </summary>
+		/// <param name="authPolicy">The default <see cref="IAuthorizationPolicy"/>s</param>
+		/// <param name="listenUri">The <see cref="Uri"/> at which the message was received.</param>
+		/// <param name="message">The <see cref="Message"/> to be authenticated</param>
+		/// <returns>A <see cref="ReadOnlyCollection{T}"/> of <see cref="IAuthorizationPolicy"/>s. Either <paramref name="authPolicy"/> or one containing a single <see cref="WindowsAuthorizationPolicy"/> if this function successfully creates one</returns>
 		public override ReadOnlyCollection<IAuthorizationPolicy> Authenticate(ReadOnlyCollection<IAuthorizationPolicy> authPolicy, Uri listenUri, ref Message message)
 		{
 			try
@@ -29,7 +38,6 @@ namespace TGS.Server.Security
 
 				if (userPosition != -1 && passPosition != -1)
 				{
-
 					var user = message.Headers.GetHeader<string>(userPosition);
 					var pass = message.Headers.GetHeader<string>(passPosition);
 
@@ -38,9 +46,9 @@ namespace TGS.Server.Security
 					var res = LogonUser(splits.Length > 1 ? splits[1] : splits[0], splits.Length > 1 ? splits[0] : null, pass, 3 /*LOGON32_LOGON_NETWORK*/, 0 /*LOGON32_PROVIDER_DEFAULT*/, out IntPtr token);
 					if (res)
 					{
-						//IMPORTANT: logoff the user after all is said and done or they'll stay logged in on the system for as long as we are
+						//IMPORTANT: logoff the user after all is said and done or they'll stay logged in on the system for as long as we are running
 						OperationContext.Current.OperationCompleted += (a, b) => { CloseHandle(token); };
-						return new ReadOnlyCollection<IAuthorizationPolicy>(new List<IAuthorizationPolicy> { new WindowsAuthorizationPolicy(token) });
+						return new ReadOnlyCollection<IAuthorizationPolicy>(new List<IAuthorizationPolicy> { new WindowsAuthorizationPolicy(new WindowsIdentity(token)) });
 					}
 				}
 			}
