b510b66cf678393b3510af2e8c0fa2e16b8da7bf
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-02-11 23:24:48 -0500

    Version bump to 3.2.1.12 [TGSDeploy]

--- AssemblyInfo.global.cs
+++ AssemblyInfo.global.cs
@@ -3,8 +3,8 @@ using System.Runtime.CompilerServices;
 
 //You cannot one definition the version number
 //Believe me, I've tried, the compiler hates it so much
-[assembly: AssemblyVersion("3.2.1.11")]
-[assembly: AssemblyFileVersion("3.2.1.11")]
-[assembly: AssemblyInformationalVersion("3.2.1.11")]
+[assembly: AssemblyVersion("3.2.1.12")]
+[assembly: AssemblyFileVersion("3.2.1.12")]
+[assembly: AssemblyInformationalVersion("3.2.1.12")]
 
 [assembly: InternalsVisibleTo("TGS.Tests")]
