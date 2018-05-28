619132d87f169e1b1e31c55733a1c79a1850fc62
Cyberboss <Cyberboss@users.noreply.github.com>
2018-05-28 11:39:44 -0400

    Adds a basic application startup test
    Remember no other I/O is allowed on a new deployment without a request

---
+++ tests/Tgstation.Server.Host.Tests/Core/TestApplication.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tgstation.Server.Host.Core.Tests
+{
+	[TestClass]
+	public sealed class TestApplication : IServerUpdateConsumer
+	{
+		public void ApplyUpdate(string updatePath) => throw new System.NotImplementedException();
+
+		[TestMethod]
+		public async Task TestSuccessfulStartup()
+		{
+			var dbName = Path.GetTempFileName();
+			try
+			{
+				using (var cts = new CancellationTokenSource())
+				using (var webHost = WebHost.CreateDefaultBuilder(new string[] { "Database:DatabaseType=Sqlite", "Database:ConnectionString=Data Source=" + dbName }) //force it to use sqlite
+					.UseStartup<Application>()
+					.Build()
+				)
+				{
+					await webHost.StartAsync().ConfigureAwait(false);
+					await webHost.StopAsync().ConfigureAwait(false);
+				}
+			}
+			finally
+			{
+				File.Delete(dbName);
+			}
+		}
+	}
+}
