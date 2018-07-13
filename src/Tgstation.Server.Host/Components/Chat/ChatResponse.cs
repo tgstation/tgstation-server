0c3aebce620d84b0de7da4cce9b1b473a36fc542
Cyberboss <Cyberboss@users.noreply.github.com>
2018-07-13 11:16:12 -0400

    Document ChatResponse

--- src/Tgstation.Server.Host/Components/Chat/ChatResponse.cs
+++ src/Tgstation.Server.Host/Components/Chat/ChatResponse.cs
@@ -2,9 +2,19 @@
 
 namespace Tgstation.Server.Host.Components.Chat
 {
+	/// <summary>
+	/// Represents a chat message requested by DD
+	/// </summary>
 	sealed class ChatResponse
 	{
+		/// <summary>
+		/// The message string
+		/// </summary>
 		public string Message { get; set; }
+
+		/// <summary>
+		/// The list of internal channel ids to send <see cref="Message"/> to
+		/// </summary>
 		public List<long> ChannelIds { get; set; }
 	}
 }
