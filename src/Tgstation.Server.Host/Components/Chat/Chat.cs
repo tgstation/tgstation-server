598d08b374d3f40696d2935b0ad8d355c4318f7c
Cyberboss <Cyberboss@users.noreply.github.com>
2018-07-13 12:02:18 -0400

    Add skeleton chat implementation

---
+++ src/Tgstation.Server.Host/Components/Chat/Chat.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Tgstation.Server.Api.Models;
+
+namespace Tgstation.Server.Host.Components.Chat
+{
+	/// <inheritdoc />
+	sealed class Chat : IChat
+	{
+		/// <inheritdoc />
+		public bool IrcConnected => throw new System.NotImplementedException();
+
+		/// <inheritdoc />
+		public bool DiscordConnected => throw new System.NotImplementedException();
+
+		/// <inheritdoc />
+		public Task ChangeChannels(IEnumerable<ChatChannel> newChannels, CancellationToken cancellationToken)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public Task ChangeSettings(Api.Models.Internal.ChatSettings newSettings, CancellationToken cancellationToken)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public Task SendMessage(string message, IEnumerable<long> channelIds, CancellationToken cancellationToken)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public Task SendWatchdogMessage(string message, CancellationToken cancellationToken)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public Task StartAsync(CancellationToken cancellationToken)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public Task StopAsync(CancellationToken cancellationToken)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public Task<IChatJsonTrackingContext> TrackJsons(string basePath, string channelsJsonName, string commandsJsonName, CancellationToken cancellationToken)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
