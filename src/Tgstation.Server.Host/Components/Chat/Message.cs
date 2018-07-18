1d931f1954914544586d252c2d5d013ebf213b48
Cyberboss <Cyberboss@users.noreply.github.com>
2018-07-18 16:37:02 -0400

    More docs and stuff

--- src/Tgstation.Server.Host/Components/Chat/Message.cs
+++ src/Tgstation.Server.Host/Components/Chat/Message.cs
@@ -1,8 +1,18 @@
 ﻿namespace Tgstation.Server.Host.Components.Chat.Providers
 {
-	public sealed class Message
+	/// <summary>
+	/// Represents a message recieved by a <see cref="IProvider"/>
+	/// </summary>
+	sealed class Message
 	{
+		/// <summary>
+		/// The text of the message
+		/// </summary>
 		public string Content { get; set; }
+
+		/// <summary>
+		/// The <see cref="Components.Chat.User"/> who sent the <see cref="Message"/>
+		/// </summary>
 		public User User { get; set; }
 	}
 }
