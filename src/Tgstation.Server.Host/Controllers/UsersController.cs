3272d3a7d764970f921a569d08aa63e51beb69d3
Cyberboss <Cyberboss@users.noreply.github.com>
2018-07-20 15:03:07 -0400

    Empty UsersController

---
+++ src/Tgstation.Server.Host/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using Tgstation.Server.Host.Models;
+using Tgstation.Server.Host.Security;
+
+namespace Tgstation.Server.Host.Controllers
+{
+	/// <summary>
+	/// For managing <see cref="User"/>s
+	/// </summary>
+	[Route("/" + nameof(Models.User))]
+	public sealed class UsersController : ModelController<Api.Models.User>
+	{
+		/// <summary>
+		/// The <see cref="ILogger"/> for the <see cref="UsersController"/>
+		/// </summary>
+		readonly ILogger<UsersController> logger;
+
+		/// <summary>
+		/// Construct a <see cref="UsersController"/>
+		/// </summary>
+		/// <param name="databaseContext">The <see cref="IDatabaseContext"/> for the <see cref="ApiController"/></param>
+		/// <param name="authenticationContextFactory">The <see cref="IAuthenticationContextFactory"/> for the <see cref="ApiController"/></param>
+		/// <param name="logger"></param>
+		public UsersController(IDatabaseContext databaseContext, IAuthenticationContextFactory authenticationContextFactory, ILogger<UsersController> logger) : base(databaseContext, authenticationContextFactory)
+		{
+			this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+	}
+}
