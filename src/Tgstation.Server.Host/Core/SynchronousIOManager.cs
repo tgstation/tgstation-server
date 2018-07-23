fc76fa28309a0651727b452934fe6cb8eeaf90a8
Cyberboss <Cyberboss@users.noreply.github.com>
2018-07-23 12:15:28 -0400

    Adds skeleton SynchronousIOManager implementation

---
+++ src/Tgstation.Server.Host/Core/SynchronousIOManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace Tgstation.Server.Host.Core
+{
+	/// <inheritdoc />
+	sealed class SynchronousIOManager : ISynchronousIOManager
+	{
+		/// <inheritdoc />
+		public IEnumerable<string> GetDirectories(string path, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public IEnumerable<string> GetFiles(string path, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public byte[] ReadFile(string path, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public bool WriteFileChecked(string path, byte[] data, string previousSha1, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
