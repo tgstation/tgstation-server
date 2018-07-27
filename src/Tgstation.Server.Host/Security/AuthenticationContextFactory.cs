6cf06636c5cd70bad4e714d0b5ade3e251305849
Cyberboss <Cyberboss@users.noreply.github.com>
2018-07-27 10:01:04 -0400

    Removes some unused usings

--- src/Tgstation.Server.Host/Security/AuthenticationContextFactory.cs
+++ src/Tgstation.Server.Host/Security/AuthenticationContextFactory.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Tgstation.Server.Host.Core;
 using Tgstation.Server.Host.Models;
 
 namespace Tgstation.Server.Host.Security
