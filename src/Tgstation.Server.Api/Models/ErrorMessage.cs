42963ada556451129964b007c64420cd83deab03
Cyberboss <Cyberboss@users.noreply.github.com>
2018-08-02 16:01:40 -0400

    Doxdoxdox

---
+++ src/Tgstation.Server.Api/Models/ErrorMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace Tgstation.Server.Api.Models
+{
+	/// <summary>
+	/// Represents an error message returned by the server
+	/// </summary>
+	public sealed class ErrorMessage
+	{
+		/// <summary>
+		/// A human readable description of the error
+		/// </summary>
+		public string Message { get; set; }
+	}
+}
