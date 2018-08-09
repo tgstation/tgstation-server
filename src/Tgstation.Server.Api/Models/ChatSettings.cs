283c7aa534a2f3acd01d6a942cdeb7484fb1c9f5
Cyberboss <Cyberboss@users.noreply.github.com>
2018-08-09 11:57:30 -0400

    Some ChatSettings sanity

--- src/Tgstation.Server.Api/Models/ChatSettings.cs
+++ src/Tgstation.Server.Api/Models/ChatSettings.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Tgstation.Server.Api.Rights;
 
 namespace Tgstation.Server.Api.Models
@@ -11,5 +13,22 @@ namespace Tgstation.Server.Api.Models
 		/// </summary>
 		[Permissions(WriteRight = ChatSettingsRights.WriteChannels)]
 		public List<ChatChannel> Channels { get; set; }
+
+		/// <summary>
+		/// Validates <see cref="Channels"/> are correct for the <see cref="Internal.ChatSettings.Provider"/>
+		/// </summary>
+		/// <returns></returns>
+		public bool ValidateProviderChannelTypes()
+		{
+			switch (Provider)
+			{
+				case ChatProvider.Discord:
+					return Channels.Select(x => x.DiscordChannelId.HasValue && x.IrcChannel == null).All(x => x);
+				case ChatProvider.Irc:
+					return Channels.Select(x => !x.DiscordChannelId.HasValue && x.IrcChannel != null).All(x => x);
+				default:
+					throw new InvalidOperationException("Invalid provider type!");
+			}
+		}
 	}
 }
