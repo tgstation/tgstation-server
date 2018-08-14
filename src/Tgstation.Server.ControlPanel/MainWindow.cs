521347e536e917a75955016aefabc432b09b7c47
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-08-13 23:37:10 -0400

    It's late, here's a control panel prototype

---
+++ src/Tgstation.Server.ControlPanel/MainWindow.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tgstation.Server.ControlPanel
+{
+	public partial class MainWindow : Form
+	{
+		readonly UserSettings userSettings;
+
+		readonly Dictionary<TreeNode, TreeNodeMouseClickEventHandler> connectionsClickHandlers;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			instanceBrowser.NodeMouseClick += InstanceBrowser_NodeMouseClick;
+
+			var rootNode = instanceBrowser.Nodes.Add("Server Connections");
+
+			connectionsClickHandlers = new Dictionary<TreeNode, TreeNodeMouseClickEventHandler>
+			{
+				{ rootNode, InstanceBrowser_RootClick }
+			};
+
+			try
+			{
+				userSettings = JsonConvert.DeserializeObject<UserSettings>(Properties.Settings.Default.SettingsJson);
+			}
+			catch (JsonSerializationException)
+			{
+				userSettings = new UserSettings();
+			}
+
+			foreach(var I in userSettings.Connections)
+			{
+				if (I.Password == null)
+					connectionsClickHandlers.Add(rootNode.Nodes.Add(I.Url + " (Sign In)"), InstanceBrowser_SignInClick);
+				else
+					connectionsClickHandlers.Add(rootNode.Nodes.Add(I.Url), InstanceBrowser_ConnectionClick);
+			}
+		}
+
+		private void InstanceBrowser_ConnectionClick(object sender, TreeNodeMouseClickEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void InstanceBrowser_SignInClick(object sender, TreeNodeMouseClickEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void InstanceBrowser_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e) => connectionsClickHandlers[e.Node](sender, e);
+
+		private void InstanceBrowser_RootClick(object sender, TreeNodeMouseClickEventArgs e)
+		{
+			if (e.Button != MouseButtons.Right)
+				return;
+			rootContextMenuStrip.Show(instanceBrowser, new Point(e.X, e.Y));
+		}
+
+		private void menuQuit_Click(object sender, EventArgs e) => Application.Exit();
+
+		private void newConnectionToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
--- src/Tgstation.Server.ControlPanel/Properties/AssemblyInfo.cs
+++ src/Tgstation.Server.ControlPanel/Properties/AssemblyInfo.cs
@@ -1,26 +1,29 @@
-﻿using System;
-using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Windows;
 
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("Tgstation.Server.ControlPanel")]
-[assembly: InternalsVisibleTo("Tgstation.Server.ControlPanel.Tests")]
+[assembly: AssemblyProduct("Tgstation.Server.ControlPanel")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-[assembly: CLSCompliant(true)]
-[assembly: NeutralResourcesLanguage("en-CA", UltimateResourceFallbackLocation.Satellite)]
 
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9a00ea43-1b87-4091-8917-df6d373ec4df")]
 
-[assembly: ThemeInfo(
-	ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-									 //(used if a resource is not found in the page,
-									 // or application resource dictionaries)
-	ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-											  //(used if a resource is not found in the page,
-											  // app, or any theme specific resource dictionaries)
-)]
-
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
 [assembly: AssemblyFileVersion("4.0.0.0")]
-[assembly: AssemblyInformationalVersion("4.0.0.0")]
