6fc3158ba9881013115d0a3a99d071e3d1888b20
Cyberboss <Cyberboss@users.noreply.github.com>
2018-08-15 10:36:48 -0400

    Removes client implementations

--- src/Tgstation.Server.ControlPanel/UserSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Tgstation.Server.ControlPanel
-{
-	sealed class UserSettings
-	{
-		public List<Connection> Connections { get; set; } = new List<Connection>();
-	}
-}
