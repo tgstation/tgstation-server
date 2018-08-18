1c989e7a0cb9ce8bcd71e306b83a26109a0a3707
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-08-18 03:46:46 -0400

    Fix stopping the Watchdog not setting Running = false

--- src/Tgstation.Server.Host/Components/Watchdog/Watchdog.cs
+++ src/Tgstation.Server.Host/Components/Watchdog/Watchdog.cs
@@ -232,6 +232,7 @@ namespace Tgstation.Server.Host.Components.Watchdog
 				var chatTask = announce ? chat.SendWatchdogMessage("Terminating...", cancellationToken) : Task.CompletedTask;
 				await StopMonitor().ConfigureAwait(false);
 				DisposeAndNullControllers();
+				Running = false;
 				await chatTask.ConfigureAwait(false);
 				return;
 			}
