20f0d2528589fcb187a40f2cc1b594f5eda04af3
Cyberboss <Cyberboss@users.noreply.github.com>
2018-08-29 09:56:42 -0400

    Clairify IJobClient documentation

--- src/Tgstation.Server.Client/Components/IJobsClient.cs
+++ src/Tgstation.Server.Client/Components/IJobsClient.cs
@@ -40,7 +40,7 @@ namespace Tgstation.Server.Client.Components
 		/// <param name="job">The <see cref="Job"/> to create a <see cref="Task"/> for</param>
 		/// <param name="requeryRate">The rate in to poll the server for results</param>
 		/// <param name="progressCallback">A <see cref="Action{T}"/> to run with 0-100 progress</param>
-		/// <param name="cancellationToken">The <see cref="CancellationToken"/> which will trigger the cancellation of the <paramref name="job"/></param>
+		/// <param name="cancellationToken">The <see cref="CancellationToken"/> for the operation. This does not cancel the <paramref name="job"/></param>
 		/// <returns>A <see cref="Task{TResult}"/> resulting in a complete <see cref="Job"/></returns>
 		Task<Job> CreateTaskFromJob(Job job, TimeSpan requeryRate, Action<int> progressCallback, CancellationToken cancellationToken);
 	}
