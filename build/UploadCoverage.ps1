a1eba117d8aaf63b721ca136fdc25668b316f85c
Cyberboss <Cyberboss@users.noreply.github.com>
2018-08-31 12:20:33 -0400

    Hopefully fixes coverage issues

--- build/UploadCoverage.ps1
+++ build/UploadCoverage.ps1
@@ -1,10 +1,10 @@
-$coverageFilePath = Resolve-Path -path "TestResults\*\*.coverage"
- 
-$coverageFilePath = $coverageFilePath.ToString()
+$coverageFilePaths = Get-ChildItem -Path TestResults -Filter *.coverage -Recurse -ErrorAction SilentlyContinue -Force | %{ $_.fullname }
+
+$coverageFilePathList = [string]$$coverageFilePaths
 
 Write-Host "Running CodeCoverage.exe..."
-&"C:\Program Files (x86)\Microsoft Visual Studio\2017\TestAgent\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" analyze /output:coverage.coveragexml "$coverageFilePath"
+&"C:\Program Files (x86)\Microsoft Visual Studio\2017\TestAgent\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" analyze /output:service.coveragexml "$coverageFilePath"
 
 rm -r TestResults
 
-codecov -f coverage.coveragexml
+codecov -f 'api_coverage.xml client_coverage.xml host_coverage.xml console_coverage.xml watchdog_coverage.xml server_coverage.xml service.coveragexml'
