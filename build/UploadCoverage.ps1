b2ae920dd876225b1ff87b24ae70e646d0e2b626
Cyberboss <Cyberboss@users.noreply.github.com>
2018-08-31 13:46:29 -0400

    Do it the dumb way then

--- build/UploadCoverage.ps1
+++ build/UploadCoverage.ps1
@@ -7,5 +7,10 @@ Write-Host "Running CodeCoverage.exe..."
 
 rm -r TestResults
 
-codecov -f './api_coverage.xml ./client_coverage.xml ./host_coverage.xml ./console_coverage.xml ./watchdog_coverage.xml ./service.coveragexml' --flag unittests
-codecov -f 'server_coverage.xml' --flag integration
+codecov -f api_coverage.xml --flag unittests
+codecov -f client_coverage.xml --flag unittests
+codecov -f host_coverage.xml  --flag unittests
+codecov -f console_coverage.xml --flag unittests
+codecov -f watchdog_coverage.xml --flag unittests
+codecov -f service.coveragexml --flag unittests
+codecov -f server_coverage.xml --flag integration
