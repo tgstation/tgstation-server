2efb77917e4260d1d6c0d5feac74b6e6ff30813b
Cyberboss <Cyberboss@users.noreply.github.com>
2018-08-31 13:23:18 -0400

    Add a simple client test

---
+++ tests/Tgstation.Server.Client.Tests/TestServerClientFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net.Http.Headers;
+
+namespace Tgstation.Server.Client.Tests
+{
+	[TestClass]
+	public sealed class TestServerClientFactory
+	{
+		[TestMethod]
+		public void TestConstruction()
+		{
+			Assert.ThrowsException<ArgumentNullException>(() => new ServerClientFactory(null));
+			new ServerClientFactory(new ProductHeaderValue("Tgstation.Server.Client.Tests", GetType().Assembly.GetName().Version.ToString()));
+		}
+	}
+}
