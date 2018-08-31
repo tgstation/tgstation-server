8eaa7dd8fbcc299103a35a44583f8f52baf4bc3e
Cyberboss <Cyberboss@users.noreply.github.com>
2018-08-31 13:37:41 -0400

    Try this?

--- build/UploadCoverage.ps1
+++ build/UploadCoverage.ps1
@@ -3,8 +3,9 @@ $coverageFilePaths = Get-ChildItem -Path TestResults -Filter *.coverage -Recurse
 $coverageFilePathList = [string]$coverageFilePaths
 
 Write-Host "Running CodeCoverage.exe..."
-&"C:\Program Files (x86)\Microsoft Visual Studio\2017\TestAgent\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" analyze /output:service.coveragexml "$coverageFilePath"
+&"C:\Program Files (x86)\Microsoft Visual Studio\2017\TestAgent\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" analyze /output:service.coveragexml "$coverageFilePathList"
 
 rm -r TestResults
 
-codecov -f 'api_coverage.xml client_coverage.xml host_coverage.xml console_coverage.xml watchdog_coverage.xml server_coverage.xml service.coveragexml'
+codecov -f './api_coverage.xml ./client_coverage.xml ./host_coverage.xml ./console_coverage.xml ./watchdog_coverage.xml ./service.coveragexml' --flag unittests
+codecov -f 'server_coverage.xml' --flag integration
