2c462c3b2173e484ec3f97273dc74ac9ef45e856
Cyberboss <Cyberboss@users.noreply.github.com>
2018-09-18 12:50:36 -0400

    Tabify existing migrations

--- src/Tgstation.Server.Host/Models/Migrations/20180918021228_MSAddMinimumSecurity.cs
+++ src/Tgstation.Server.Host/Models/Migrations/20180918021228_MSAddMinimumSecurity.cs
@@ -13,33 +13,33 @@ namespace Tgstation.Server.Host.Migrations
 		/// </summary>
 		/// <param name="migrationBuilder">The <see cref="MigrationBuilder"/> to use</param>
 		protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<int>(
-                name: "ApiValidationSecurityLevel",
-                table: "DreamMakerSettings",
-                nullable: false,
-                defaultValue: (int)DreamDaemonSecurity.Safe);
+		{
+			migrationBuilder.AddColumn<int>(
+				name: "ApiValidationSecurityLevel",
+				table: "DreamMakerSettings",
+				nullable: false,
+				defaultValue: (int)DreamDaemonSecurity.Safe);
 
-            migrationBuilder.AddColumn<int>(
-                name: "MinimumSecurityLevel",
-                table: "CompileJobs",
-                nullable: false,
-                defaultValue: (int)DreamDaemonSecurity.Safe);
-        }
+			migrationBuilder.AddColumn<int>(
+				name: "MinimumSecurityLevel",
+				table: "CompileJobs",
+				nullable: false,
+				defaultValue: (int)DreamDaemonSecurity.Safe);
+		}
 
 		/// <summary>
 		/// Unapplies the migration
 		/// </summary>
 		/// <param name="migrationBuilder">The <see cref="MigrationBuilder"/> to use</param>
 		protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "ApiValidationSecurityLevel",
-                table: "DreamMakerSettings");
+		{
+			migrationBuilder.DropColumn(
+				name: "ApiValidationSecurityLevel",
+				table: "DreamMakerSettings");
 
-            migrationBuilder.DropColumn(
-                name: "MinimumSecurityLevel",
-                table: "CompileJobs");
-        }
-    }
+			migrationBuilder.DropColumn(
+				name: "MinimumSecurityLevel",
+				table: "CompileJobs");
+		}
+	}
 }
