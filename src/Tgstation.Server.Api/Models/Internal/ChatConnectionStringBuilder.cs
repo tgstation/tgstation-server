962d08f9a6ed8f6d7413949298e9855d75941cf8
Cyberboss <Cyberboss@users.noreply.github.com>
2018-09-21 11:21:35 -0400

    Add ChatConnectionStringBuilder
    A way to more publically display how to create chat bot connection strings

---
+++ src/Tgstation.Server.Api/Models/Internal/ChatConnectionStringBuilder.cs
@@ -0,0 +1,19 @@
+﻿namespace Tgstation.Server.Api.Models.Internal
+{
+	/// <summary>
+	/// Helper for building <see cref="ChatBot.ConnectionString"/>s
+	/// </summary>
+	public abstract class ChatConnectionStringBuilder
+	{
+		/// <summary>
+		/// If the <see cref="ChatConnectionStringBuilder"/> evaluates to a valid <see cref="ChatBot.ConnectionString"/>
+		/// </summary>
+		public abstract bool Valid { get; }
+
+		/// <summary>
+		/// Gets the <see cref="ChatBot.ConnectionString"/> associated with the <see cref="ChatConnectionStringBuilder"/>
+		/// </summary>
+		/// <returns></returns>
+		public abstract override string ToString();
+	}
+}
