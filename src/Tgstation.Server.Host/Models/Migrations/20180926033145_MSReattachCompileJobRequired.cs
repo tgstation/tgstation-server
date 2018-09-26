dd046bc0a40f099ac8fc6eff3df4d9d90dcadf43
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-09-25 23:43:07 -0400

    Migrations for the ReattachInformation CompileJob requirement

---
+++ src/Tgstation.Server.Host/Models/Migrations/20180926033145_MSReattachCompileJobRequired.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tgstation.Server.Host.Models.Migrations
+{
+	/// <summary>
+	/// Marks the <see cref="CompileJob"/>s of <see cref="ReattachInformation"/>s as non-nullable for MSSQL
+	/// </summary>
+	public partial class MSReattachCompileJobRequired : Migration
+	{
+		/// <summary>
+		/// Applies the migration
+		/// </summary>
+		/// <param name="migrationBuilder">The <see cref="MigrationBuilder"/> to use</param>
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.DropForeignKey(
+				name: "FK_ReattachInformations_CompileJobs_CompileJobId",
+				table: "ReattachInformations");
+
+			migrationBuilder.AlterColumn<long>(
+				name: "CompileJobId",
+				table: "ReattachInformations",
+				nullable: false,
+				oldClrType: typeof(long),
+				oldNullable: true);
+
+			migrationBuilder.AddForeignKey(
+				name: "FK_ReattachInformations_CompileJobs_CompileJobId",
+				table: "ReattachInformations",
+				column: "CompileJobId",
+				principalTable: "CompileJobs",
+				principalColumn: "Id",
+				onDelete: ReferentialAction.Cascade);
+		}
+
+		/// <summary>
+		/// Unapplies the migration
+		/// </summary>
+		/// <param name="migrationBuilder">The <see cref="MigrationBuilder"/> to use</param>
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.DropForeignKey(
+				name: "FK_ReattachInformations_CompileJobs_CompileJobId",
+				table: "ReattachInformations");
+
+			migrationBuilder.AlterColumn<long>(
+				name: "CompileJobId",
+				table: "ReattachInformations",
+				nullable: true,
+				oldClrType: typeof(long));
+
+			migrationBuilder.AddForeignKey(
+				name: "FK_ReattachInformations_CompileJobs_CompileJobId",
+				table: "ReattachInformations",
+				column: "CompileJobId",
+				principalTable: "CompileJobs",
+				principalColumn: "Id",
+				onDelete: ReferentialAction.Restrict);
+		}
+	}
+}
