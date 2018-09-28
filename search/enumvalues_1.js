2c90e780164298071b01fdd05395ad2215514636
tgstation-server <tgstation-server@tgstation13.org>
2018-09-28 18:05:45 +0000

    Deploy code docs to GitHub Pages for Appveyor build 988
    Commit: d1f36fece6f715bc85bd5c468618efea10b8a1b7

--- search/enumvalues_1.js
+++ search/enumvalues_1.js
@@ -3,7 +3,7 @@ var searchData=
   ['badvalidationrequest',['BadValidationRequest',['../namespace_tgstation_1_1_server_1_1_host_1_1_components_1_1_watchdog.html#af7163ca98f9f5e5c731043ecdace3745a18d2471b9e1234b2d15002cbafa2adc2',1,'Tgstation::Server::Host::Components::Watchdog']]],
   ['break',['Break',['../namespace_tgstation_1_1_server_1_1_host_1_1_components_1_1_watchdog.html#a97c1a4a168451bd7e3be23c467aa2812ae4c38d6d45baf080943d323dd51a2ce8',1,'Tgstation::Server::Host::Components::Watchdog']]],
   ['byond',['Byond',['../namespace_tgstation_1_1_server_1_1_api_1_1_rights.html#a7b02c8c3dadd90810a9eaa24712a0a20a2f4b44b678b37bcd3239cd83e64d0eb0',1,'Tgstation::Server::Api::Rights']]],
-  ['byondchangecomplete',['ByondChangeComplete',['../namespace_tgstation_1_1_server_1_1_host_1_1_components.html#a67a04fc5ffb3b43b0f887df8796d6381ab59e38171b7de5f08e79ad7c4b8b67e2',1,'Tgstation::Server::Host::Components']]],
-  ['byondchangestart',['ByondChangeStart',['../namespace_tgstation_1_1_server_1_1_host_1_1_components.html#a67a04fc5ffb3b43b0f887df8796d6381a8eee33fb4d22679ea77c0e51fcd94059',1,'Tgstation::Server::Host::Components']]],
-  ['byondfail',['ByondFail',['../namespace_tgstation_1_1_server_1_1_host_1_1_components.html#a67a04fc5ffb3b43b0f887df8796d6381a138c9788d2e2957de757e51c66e3f172',1,'Tgstation::Server::Host::Components']]]
+  ['byondactiveversionchange',['ByondActiveVersionChange',['../namespace_tgstation_1_1_server_1_1_host_1_1_components.html#a67a04fc5ffb3b43b0f887df8796d6381a16551fe43323cca07bfc777afadbe5a1',1,'Tgstation::Server::Host::Components']]],
+  ['byondinstallfail',['ByondInstallFail',['../namespace_tgstation_1_1_server_1_1_host_1_1_components.html#a67a04fc5ffb3b43b0f887df8796d6381aa63abc793fbdf14cf7caf28a4812e682',1,'Tgstation::Server::Host::Components']]],
+  ['byondinstallstart',['ByondInstallStart',['../namespace_tgstation_1_1_server_1_1_host_1_1_components.html#a67a04fc5ffb3b43b0f887df8796d6381a93c1e81c6ae4a9cc3637be6b044211e9',1,'Tgstation::Server::Host::Components']]]
 ];
--- search/enumvalues_3.js
+++ search/enumvalues_3.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['ddothercrash',['DDOtherCrash',['../namespace_tgstation_1_1_server_1_1_host_1_1_components.html#a67a04fc5ffb3b43b0f887df8796d6381a5ba6633142a289b4df4f63fbe884488d',1,'Tgstation::Server::Host::Components']]],
-  ['ddotherexit',['DDOtherExit',['../namespace_tgstation_1_1_server_1_1_host_1_1_components.html#a67a04fc5ffb3b43b0f887df8796d6381a93960808163206d073bddea27a19df79',1,'Tgstation::Server::Host::Components']]],
   ['delete',['Delete',['../namespace_tgstation_1_1_server_1_1_api_1_1_rights.html#a580a36a11165b2590be342b45c31ec72af2a6c498fb90ee345d997f888fce3b18',1,'Tgstation.Server.Api.Rights.Delete()'],['../namespace_tgstation_1_1_server_1_1_api_1_1_rights.html#a691a59767073bc7dff0f17b809d07b0caf2a6c498fb90ee345d997f888fce3b18',1,'Tgstation.Server.Api.Rights.Delete()'],['../namespace_tgstation_1_1_server_1_1_api_1_1_rights.html#af199a84ed6fe6ca557db9de91070ad79af2a6c498fb90ee345d997f888fce3b18',1,'Tgstation.Server.Api.Rights.Delete()'],['../namespace_tgstation_1_1_server_1_1_api_1_1_rights.html#a95336db3db40dbae9701ef6e51240f39af2a6c498fb90ee345d997f888fce3b18',1,'Tgstation.Server.Api.Rights.Delete()']]],
   ['disallowed',['Disallowed',['../namespace_tgstation_1_1_server_1_1_api_1_1_models.html#ab7bc96f211e450615be8669d10de2440ade99ae8770699a4725a85853a0790d24',1,'Tgstation::Server::Api::Models']]],
   ['discord',['Discord',['../namespace_tgstation_1_1_server_1_1_api_1_1_models.html#a2d962413bc23404156feea75066e39cca8f5cc6430613f1c12f36965050bb7197',1,'Tgstation::Server::Api::Models']]],
