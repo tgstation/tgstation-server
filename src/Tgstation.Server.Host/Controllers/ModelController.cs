e5b3e450e3fc9c0d20c7d8aab8e75e75c301b861
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-09-30 12:10:29 -0400

    Remove unused usings

--- src/Tgstation.Server.Host/Controllers/ModelController.cs
+++ src/Tgstation.Server.Host/Controllers/ModelController.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Tgstation.Server.Api;
 using Tgstation.Server.Host.Models;
 using Tgstation.Server.Host.Security;
 
