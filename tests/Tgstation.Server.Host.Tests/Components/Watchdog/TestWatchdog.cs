3596383532f4635d2e3bc3bfc79031190e0e7e02
Cyberboss <Cyberboss@users.noreply.github.com>
2018-10-02 15:40:54 -0400

    Add a basic watchdog test

--- tests/Tgstation.Server.Host.Tests/Components/Watchdog/TestWatchdog.cs
+++ tests/Tgstation.Server.Host.Tests/Components/Watchdog/TestWatchdog.cs
@@ -3,6 +3,9 @@ using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Tgstation.Server.Api.Models.Internal;
 using Tgstation.Server.Host.Components.Chat;
 using Tgstation.Server.Host.Components.Compiler;
@@ -58,6 +61,80 @@ namespace Tgstation.Server.Host.Components.Watchdog.Tests
 			var mockInstance = new Models.Instance();
 			new Watchdog(mockChat.Object, mockSessionControllerFactory.Object, mockDmbFactory.Object, mockLogger.Object, mockReattachInfoHandler.Object, mockDatabaseContextFactory.Object, mockByondTopicSender.Object, mockEventConsumer.Object, mockJobManager.Object, mockServerControl.Object, mockLaunchParameters, mockInstance, default).Dispose();
 
+			mockRestartRegistration.VerifyAll();
+			mockServerControl.VerifyAll();
+			mockChat.VerifyAll();
+		}
+
+		[TestMethod]
+		public async Task TestSuccessfulLaunchAndShutdown()
+		{
+			var mockChat = new Mock<IChat>();
+			mockChat.Setup(x => x.RegisterCommandHandler(It.IsNotNull<ICustomCommandHandler>())).Verifiable();
+			var mockSessionControllerFactory = new Mock<ISessionControllerFactory>();
+			var mockDmbFactory = new Mock<IDmbFactory>();
+			var mockLogger = new Mock<ILogger<Watchdog>>();
+			var mockReattachInfoHandler = new Mock<IReattachInfoHandler>();
+			var mockDatabaseContextFactory = new Mock<IDatabaseContextFactory>();
+			var mockByondTopicSender = new Mock<IByondTopicSender>();
+			var mockEventConsumer = new Mock<IEventConsumer>();
+			var mockJobManager = new Mock<IJobManager>();
+			var mockRestartRegistration = new Mock<IRestartRegistration>();
+			mockRestartRegistration.Setup(x => x.Dispose()).Verifiable();
+			var mockServerControl = new Mock<IServerControl>();
+			mockServerControl.Setup(x => x.RegisterForRestart(It.IsNotNull<IRestartHandler>())).Returns(mockRestartRegistration.Object).Verifiable();
+			var mockLaunchParameters = new DreamDaemonLaunchParameters();
+			var mockInstance = new Models.Instance();
+
+			using (var wd = new Watchdog(mockChat.Object, mockSessionControllerFactory.Object, mockDmbFactory.Object, mockLogger.Object, mockReattachInfoHandler.Object, mockDatabaseContextFactory.Object, mockByondTopicSender.Object, mockEventConsumer.Object, mockJobManager.Object, mockServerControl.Object, mockLaunchParameters, mockInstance, default))
+			using (var cts = new CancellationTokenSource())
+			{
+				var mockCompileJob = new Models.CompileJob();
+				var mockDmbProvider = new Mock<IDmbProvider>();
+				mockDmbProvider.SetupGet(x => x.CompileJob).Returns(mockCompileJob).Verifiable();
+				var mDmbP = mockDmbProvider.Object;
+
+				var infiniteTask = new TaskCompletionSource<int>().Task;
+
+				mockDmbFactory.SetupGet(x => x.OnNewerDmb).Returns(infiniteTask);
+				mockDmbFactory.Setup(x => x.LockNextDmb(2)).Returns(mDmbP).Verifiable();
+
+				var sessionsToVerify = new List<Mock<ISessionController>>();
+
+				var cancellationToken = cts.Token;
+				mockSessionControllerFactory.Setup(x => x.LaunchNew(mockLaunchParameters, mDmbP, null, It.IsAny<bool>(), It.IsAny<bool>(), false, cancellationToken)).Returns(() =>
+				{
+					var mockSession = new Mock<ISessionController>();
+					mockSession.SetupGet(x => x.Lifetime).Returns(infiniteTask).Verifiable();
+					mockSession.SetupGet(x => x.OnReboot).Returns(infiniteTask).Verifiable();
+					mockSession.SetupGet(x => x.Dmb).Returns(mDmbP).Verifiable();
+					mockSession.SetupGet(x => x.LaunchResult).Returns(Task.FromResult(new LaunchResult
+					{
+						StartupTime = TimeSpan.FromSeconds(1)
+					})).Verifiable();
+					sessionsToVerify.Add(mockSession);
+					return Task.FromResult(mockSession.Object);
+				}).Verifiable();
+
+				cts.CancelAfter(TimeSpan.FromSeconds(15));
+
+				try
+				{
+					await wd.Launch(cancellationToken).ConfigureAwait(false);
+					await wd.Terminate(false, cancellationToken).ConfigureAwait(false);
+				}
+				finally
+				{
+					cts.Cancel();
+				}
+				Assert.AreEqual(2, sessionsToVerify.Count);
+				foreach (var I in sessionsToVerify)
+					I.VerifyAll();
+				mockDmbProvider.VerifyAll();
+			}
+
+			mockSessionControllerFactory.VerifyAll();
+			mockDmbFactory.VerifyAll();
 			mockRestartRegistration.VerifyAll();
 			mockServerControl.VerifyAll();
 			mockChat.VerifyAll();
