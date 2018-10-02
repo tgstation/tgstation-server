924b5299ce20d0adc21f5b2c3ba7ac21baf51513
Cyberboss <Cyberboss@users.noreply.github.com>
2018-10-02 11:07:57 -0400

    Removes DEBUG restriction on SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE

--- src/Tgstation.Server.Host/IO/WindowsSymlinkFactory.cs
+++ src/Tgstation.Server.Host/IO/WindowsSymlinkFactory.cs
@@ -15,10 +15,7 @@ namespace Tgstation.Server.Host.IO
 		public Task CreateSymbolicLink(string targetPath, string linkPath, CancellationToken cancellationToken) => Task.Factory.StartNew(() =>
 		{
 			//check if its not a file
-			var flags = File.Exists(targetPath) ? 0 : 1; //SYMBOLIC_LINK_FLAG_DIRECTORY
-#if DEBUG
-			flags |= 2; //SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE on win10 1607+ developer mode
-#endif
+			var flags = File.Exists(targetPath) ? 0 : 3; //SYMBOLIC_LINK_FLAG_DIRECTORY | SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE on win10 1607+ developer mode
 
 			cancellationToken.ThrowIfCancellationRequested();
 			if (!NativeMethods.CreateSymbolicLink(linkPath, targetPath, flags))
