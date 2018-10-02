258a24dfcade05bcf37c1cb4274b3337dbc8813b
Cyberboss <Cyberboss@users.noreply.github.com>
2018-10-02 12:14:18 -0400

    Adds TestJobException

---
+++ tests/Tgstation.Server.Host.Tests/Core/TestJobException.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tgstation.Server.Host.Core.Tests
+{
+	[TestClass]
+	public sealed class TestJobException
+	{
+		[TestMethod]
+		public void TestConstruction()
+		{
+			new JobException();
+			new JobException("Message");
+			new JobException("Message", new Exception());
+		}
+	}
+}
