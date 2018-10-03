58aa783e7cfec3e40538903ee6d1a7c0f7c8ac66
Cyberboss <Cyberboss@users.noreply.github.com>
2018-10-03 09:44:40 -0400

    Remove unused using

--- src/Tgstation.Server.Host/Core/Application.cs
+++ src/Tgstation.Server.Host/Core/Application.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
