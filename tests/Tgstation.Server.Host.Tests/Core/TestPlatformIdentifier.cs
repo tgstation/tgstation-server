8b9496166e289a8ad051264817e69d32731bab7a
Cyberboss <Cyberboss@users.noreply.github.com>
2018-10-15 10:40:28 -0400

    Add TestPlatformIdentifier

---
+++ tests/Tgstation.Server.Host.Tests/Core/TestPlatformIdentifier.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.InteropServices;
+
+namespace Tgstation.Server.Host.Core.Tests
+{
+	[TestClass]
+	public sealed class TestPlatformIdentifier
+	{
+		[TestMethod]
+		public void TestCorrectPlatform()
+		{
+			var identifier = new PlatformIdentifier();
+			Assert.AreEqual(RuntimeInformation.IsOSPlatform(OSPlatform.Windows), identifier.IsWindows);
+		}
+	}
+}
