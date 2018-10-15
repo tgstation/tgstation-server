f9064cb2ccbc6be4e39a1ca4d3a4a9f24e9597d5
Cyberboss <Cyberboss@users.noreply.github.com>
2018-10-15 10:38:49 -0400

    Add PlatformIdentifier

---
+++ src/Tgstation.Server.Host/Core/PlatformIdentifier.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Tgstation.Server.Host.Core
+{
+	/// <inheritdoc />
+	sealed class PlatformIdentifier : IPlatformIdentifier
+	{
+		/// <inheritdoc />
+		public bool IsWindows { get; }
+
+		/// <summary>
+		/// Construct a <see cref="PlatformIdentifier"/>
+		/// </summary>
+		public PlatformIdentifier()
+		{
+			IsWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+		}
+	}
+}
