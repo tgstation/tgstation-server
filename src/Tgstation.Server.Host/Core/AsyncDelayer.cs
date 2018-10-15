a58fa143b189061997f87c77dd5f44bdc1f9d655
Cyberboss <Cyberboss@users.noreply.github.com>
2018-10-15 11:08:08 -0400

    Add AsyncDelayer

---
+++ src/Tgstation.Server.Host/Core/AsyncDelayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tgstation.Server.Host.Core
+{
+	/// <inheritdoc />
+	sealed class AsyncDelayer : IAsyncDelayer
+	{
+		/// <inheritdoc />
+		public Task Delay(TimeSpan timeSpan, CancellationToken cancellationToken) => Task.Delay(timeSpan, cancellationToken);
+	}
+}
