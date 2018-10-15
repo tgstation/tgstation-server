b51f147cd8f5153131a4efd7f989127be445632d
Cyberboss <Cyberboss@users.noreply.github.com>
2018-10-15 10:38:42 -0400

    Add IPlatformIdentifier

---
+++ src/Tgstation.Server.Host/Core/IPlatformIdentifier.cs
@@ -0,0 +1,13 @@
+﻿namespace Tgstation.Server.Host.Core
+{
+	/// <summary>
+	/// For identifying the current platform
+	/// </summary>
+	interface IPlatformIdentifier
+	{
+		/// <summary>
+		/// If the current platform is a Windows platform
+		/// </summary>
+		bool IsWindows { get; }
+	}
+}
