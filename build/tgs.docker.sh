077e62552911d2623e7acff24219c00f094e11bc
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-10-16 22:17:15 -0400

    Fix setup wizard not running on docker version

--- build/tgs.docker.sh
+++ build/tgs.docker.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+touch /config_data/appsettings.Production.json
 ln -s /config_data/appsettings.Production.json /app/appsettings.Production.json
 
 exec dotnet Tgstation.Server.Host.Console.dll "$@"
