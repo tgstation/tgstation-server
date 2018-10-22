ac35870b0b4e1c5ddb53c9e03c29bc0a07f086b7
Cyberboss <Cyberboss@users.noreply.github.com>
2018-10-22 12:41:33 -0400

    Get it presentable

--- src/Tgstation.Server.Host/ClientApp/src/index.tsx
+++ src/Tgstation.Server.Host/ClientApp/src/index.tsx
@@ -3,6 +3,8 @@ import * as ReactDOM from 'react-dom';
 import ControlPanel from 'tgstation-server-control-panel';
 import registerServiceWorker from './registerServiceWorker';
 
+import './index.css';
+
 const contentElement = document.getElementById('root') as HTMLElement;
 const legacyUserLanguageKey = 'userLanguage';
 const userLang = navigator.language || navigator[legacyUserLanguageKey];
