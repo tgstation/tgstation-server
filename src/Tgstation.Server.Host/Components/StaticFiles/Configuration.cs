67287458d3b907bf04f3e51d40129b98a4cce309
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-10-24 12:13:56 -0400

    Don't post write on deletes

--- src/Tgstation.Server.Host/Components/StaticFiles/Configuration.cs
+++ src/Tgstation.Server.Host/Components/StaticFiles/Configuration.cs
@@ -366,7 +366,8 @@ namespace Tgstation.Server.Host.Components.StaticFiles
 						var success = synchronousIOManager.WriteFileChecked(path, data, ref fileHash, cancellationToken);
 						if (!success)
 							return;
-						postWriteHandler.HandleWrite(path);						
+						if (data != null)
+							postWriteHandler.HandleWrite(path);						
 						result = new ConfigurationFile
 						{
 							Content = data,
