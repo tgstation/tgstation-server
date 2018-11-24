d8b7eb08281029f7026bb67f97d20b40be2eecad
steamport <steamport@protonmail.com>
2018-11-24 18:21:48 -0500

    And re-add it!

--- src/Tgstation.Server.Host/Models/Migrations/20181124231534_MSToggleTestmergeComments.Designer.cs
+++ src/Tgstation.Server.Host/Models/Migrations/20181124231534_MSToggleTestmergeComments.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Tgstation.Server.Host.Models;
 
-namespace Tgstation.Server.Host.Models.Migrations
+namespace Tgstation.Server.Host.Migrations.SqlServerDatabase
 {
     [DbContext(typeof(SqlServerDatabaseContext))]
-    partial class SqlServerDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20181124231534_MSToggleTestmergeComments")]
+    partial class MSToggleTestmergeComments
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Tgstation.Server.Host/Models/Migrations/20181124231549_MYToggleTestmergeComments.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tgstation.Server.Host.Models.Migrations
+{
+	/// <summary>
+	/// Add the <see cref="Api.Models.Internal.RepositorySettings.PostTestMergeComment"/> column for MySQL/MariaDB
+	/// </summary>
+	public partial class MYToggleTestmergeComments : Migration
+	{
+		/// <summary>
+		/// Applies the migration
+		/// </summary>
+		/// <param name="migrationBuilder">The <see cref="MigrationBuilder"/> to use</param>
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.AddColumn<bool>(
+				name: "PostTestMergeComment",
+				table: "RepositorySettings",
+				nullable: false,
+				defaultValue: true);
+		}
+
+		/// <summary>
+		/// Unapplies the migration
+		/// </summary>
+		/// <param name="migrationBuilder">The <see cref="MigrationBuilder"/> to use</param>
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.DropColumn(
+				name: "PostTestMergeComment",
+				table: "RepositorySettings");
+		}
+	}
+}
