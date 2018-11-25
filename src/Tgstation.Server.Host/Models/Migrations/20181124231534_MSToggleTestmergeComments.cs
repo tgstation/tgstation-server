358eb61e8bb4d6189c530602f5e6205b32ac213a
steamport <steamport@protonmail.com>
2018-11-24 20:07:27 -0500

    Handle remaining reviews

--- src/Tgstation.Server.Host/Models/Migrations/20181124231534_MSToggleTestmergeComments.cs
+++ src/Tgstation.Server.Host/Models/Migrations/20181124231534_MSToggleTestmergeComments.cs
@@ -6,29 +6,29 @@ namespace Tgstation.Server.Host.Models.Migrations
 	/// Add the <see cref="Api.Models.Internal.RepositorySettings.PostTestMergeComment"/> column for MSSQL
 	/// </summary>
 	public partial class MSToggleTestmergeComments : Migration
-    {
+	{
 		/// <summary>
 		/// Applies the migration
 		/// </summary>
 		/// <param name="migrationBuilder">The <see cref="MigrationBuilder"/> to use</param>
 		protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<bool>(
-                name: "PostTestMergeComment",
-                table: "RepositorySettings",
-                nullable: false,
-                defaultValue: true);
-        }
+		{
+			migrationBuilder.AddColumn<bool>(
+				name: "PostTestMergeComment",
+				table: "RepositorySettings",
+				nullable: false,
+				defaultValue: true);
+		}
 
 		/// <summary>
 		/// Unapplies the migration
 		/// </summary>
 		/// <param name="migrationBuilder">The <see cref="MigrationBuilder"/> to use</param>
 		protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "PostTestMergeComment",
-                table: "RepositorySettings");
-        }
-    }
+		{
+			migrationBuilder.DropColumn(
+				name: "PostTestMergeComment",
+				table: "RepositorySettings");
+		}
+	}
 }
