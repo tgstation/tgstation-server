ae880389fe4b38087c03f50dd9cae2acfb64d885
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-11-26 14:43:22 -0500

    Just use a script actually

---
+++ build/prep_deployment.ps1
@@ -0,0 +1,19 @@
+$env:TGSVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$env:APPVEYOR_BUILD_FOLDER/artifacts/ServerHost/Tgstation.Server.Host.dll").FileVersion
+if (($env:CONFIGURATION -match "Release") -And ($env:APPVEYOR_REPO_BRANCH -match "master")) {
+    if ($env:APPVEYOR_REPO_COMMIT_MESSAGE -match "\[TGSDeploy\]") {
+        $env:TGSDeploy = "Do it." 
+    }
+    if ($env:APPVEYOR_REPO_COMMIT_MESSAGE -match "\[NugetDeploy\]") {
+        $env:NugetDeploy = "Do it."
+    }
+}
+
+dotnet run -p tools/ReleaseNotes $env:TGSVersion --no-close
+$env:TGSGoodNotes = $?
+$releaseNotesPath = "tools/ReleaseNotes/release_notes.md"
+if (Test-Path $path -PathType $releaseNotesPath) {
+    $env:TGSReleaseNotes = [IO.File]::ReadAllText($releaseNotesPath)
+}
+else {
+    $env:TGSReleaseNotes = "Automatic generation failed, please fill manually!"
+}
