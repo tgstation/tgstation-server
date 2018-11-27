0f7abbf4c31f18f189cfab78c6b6d2e7395619c6
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-11-27 16:07:42 -0500

    Better initial touch of production config in docker image

--- build/tgs.docker.sh
+++ build/tgs.docker.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-touch /config_data/appsettings.Production.json
-ln -s /config_data/appsettings.Production.json /app/appsettings.Production.json
+PROD_CONFIG=/config_data/appsettings.Production.json
+if [ ! -f $PROD_CONFIG ]; then
+	echo "{}" > $PROD_CONFIG
+fi
+ln -s $PROD_CONFIG /app/appsettings.Production.json
 
 exec dotnet Tgstation.Server.Host.Console.dll "$@"
