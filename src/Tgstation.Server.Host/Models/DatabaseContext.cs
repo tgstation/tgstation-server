73e0c9a8e48e8baf91e023b995a2e58fd74aeb14
steamport <steamport@protonmail.com>
2018-12-03 17:44:05 -0500

    no u

--- src/Tgstation.Server.Host/Models/DatabaseContext.cs
+++ src/Tgstation.Server.Host/Models/DatabaseContext.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Tgstation.Server.Host.Configuration;
+using Tgstation.Server.Host.Models.Migrations;
 
 namespace Tgstation.Server.Host.Models
 {
