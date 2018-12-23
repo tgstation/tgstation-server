1e94f8d793c85b62d286d2046a361ed4a4fd36ba
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-12-23 13:28:12 -0500

    Minor version bumps do not break API

--- src/Tgstation.Server.Api/ApiHeaders.cs
+++ src/Tgstation.Server.Api/ApiHeaders.cs
@@ -95,7 +95,7 @@ namespace Tgstation.Server.Api
 		/// </summary>
 		/// <param name="otherVersion">The <see cref="Version"/> to test</param>
 		/// <returns><see langword="true"/> if the given version is compatible with the API. <see langword="false"/> otherwise</returns>
-		public static bool CheckCompatibility(Version otherVersion) => !(Version.Major != otherVersion.Major || Version.Minor != otherVersion.Minor || Version.Build > otherVersion.Build);
+		public static bool CheckCompatibility(Version otherVersion) => !(Version.Major != otherVersion.Major || Version.Minor > otherVersion.Minor);
 
 		/// <summary>
 		/// Construct <see cref="ApiHeaders"/> for JWT authentication
