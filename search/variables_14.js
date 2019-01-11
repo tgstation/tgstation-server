87d201c68d8fcbea845bdc53e164fb355d2bf42a
tgstation-server <tgstation-server@tgstation13.org>
2019-01-11 21:46:57 +0000

    Deploy code docs to GitHub Pages for Appveyor build 1056
    Commit: 46d7c7f881aa262abde6a932c4ac52f22e4ef50f

--- search/variables_14.js
+++ search/variables_14.js
@@ -7,6 +7,7 @@ var searchData=
   ['updating',['updating',['../class_tgstation_1_1_server_1_1_host_1_1_server.html#a3a4cc72abe0c0a1e8caddeab4a210324',1,'Tgstation::Server::Host::Server']]],
   ['url',['Url',['../class_tgstation_1_1_server_1_1_client_1_1_server_client.html#a7dcb54f317c73bd5a54f7856f0ef79fc',1,'Tgstation::Server::Client::ServerClient']]],
   ['user',['User',['../class_tgstation_1_1_server_1_1_api_1_1_routes.html#a12784e8b5b7ac25c0684c9d4c7c59178',1,'Tgstation::Server::Api::Routes']]],
+  ['useragent',['UserAgent',['../class_tgstation_1_1_server_1_1_api_1_1_api_headers.html#a344ee837192b944640d63d1070ba4f2d',1,'Tgstation::Server::Api::ApiHeaders']]],
   ['username',['Username',['../class_tgstation_1_1_server_1_1_host_1_1_security_1_1_windows_system_identity.html#a7bbd8a3a43a6a2864b3348ae7f9438b9',1,'Tgstation::Server::Host::Security::WindowsSystemIdentity']]],
   ['usernameheader',['UsernameHeader',['../class_tgstation_1_1_server_1_1_api_1_1_api_headers.html#aeb78db4fde83cb73bbad383ccb1b71f1',1,'Tgstation::Server::Api::ApiHeaders']]],
   ['userprincipal',['userPrincipal',['../class_tgstation_1_1_server_1_1_host_1_1_security_1_1_windows_system_identity.html#abf93d22395acd37f9c24aee156691f7a',1,'Tgstation::Server::Host::Security::WindowsSystemIdentity']]]
