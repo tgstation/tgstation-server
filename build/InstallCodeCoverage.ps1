bfc9a90aabac7d4e35fd38c0deb94480246fc5be
Jordan Brown <Cyberboss@users.noreply.github.com>
2019-12-31 10:13:22 -0500

    Use symlinks for CodeCoverage.exe

--- build/InstallCodeCoverage.ps1
+++ build/InstallCodeCoverage.ps1
@@ -1,9 +1,9 @@
-mkdir Temp
-cd Temp
-nuget install Microsoft.CodeCoverage -Version 16.4.0
-cd ..
+New-Item -Type Directory -Force -Path "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Team Tools\Dynamic Code Coverage Tools"
 
-New-Item -Type Directory -Force -Path "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Team Tools"
-Move-Item -Path "Temp\Microsoft.CodeCoverage.16.4.0\build\netstandard1.0\CodeCoverage" -Destination "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Team Tools\Dynamic Code Coverage Tools"
-Move-Item -Path "Temp\Microsoft.CodeCoverage.16.4.0\lib" -Destination "C:\Program Files (x86)\Microsoft Visual Studio\2019\lib"
-Remove-Item -Recurse -Force Temp
+mkdir C:\CodeCoverage
+pushd C:\CodeCoverage
+nuget install Microsoft.CodeCoverage -Version 16.4.0
+cmd /c mklink "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" "Microsoft.CodeCoverage.16.4.0\build\netstandard1.0\CodeCoverage\CodeCoverage.exe"
+mklink c:\path\to\symlink c:\target\file
+popd
+
