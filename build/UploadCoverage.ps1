9f3a0756ad28d3a418bae62367017c79f8584ba6
Jordan Brown <Cyberboss@users.noreply.github.com>
2019-12-31 10:32:53 -0500

    Remove the stupid cleanup

--- build/UploadCoverage.ps1
+++ build/UploadCoverage.ps1
@@ -5,8 +5,6 @@ $coverageFilePathList = [string]$coverageFilePaths
 Write-Host "Running CodeCoverage.exe on $coverageFilePathList"
 &"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" analyze /output:service.coveragexml "$coverageFilePathList"
 
-rm -r TestResults
-
 codecov -f api_coverage.xml --flag unittests
 codecov -f client_coverage.xml --flag unittests
 codecov -f host_coverage.xml  --flag unittests
