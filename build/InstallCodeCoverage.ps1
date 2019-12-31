38abbb10ef6bd0926c9e7cbb526c85d7f612c211
Jordan Brown <Cyberboss@users.noreply.github.com>
2019-12-31 10:43:15 -0500

    Fuck it, who needs coverage on a service?

--- build/InstallCodeCoverage.ps1
+++
@@ -1,8 +0,0 @@
-New-Item -Type Directory -Force -Path "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Team Tools\Dynamic Code Coverage Tools"
-
-mkdir C:\CodeCoverage
-pushd C:\CodeCoverage
-nuget install Microsoft.CodeCoverage -Version 16.4.0
-cmd /c mklink "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" "Microsoft.CodeCoverage.16.4.0\build\netstandard1.0\CodeCoverage\CodeCoverage.exe"
-popd
-
