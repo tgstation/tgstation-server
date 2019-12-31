c8913d3af766e20bf6358df563e7601436446433
Jordan Brown <Cyberboss@users.noreply.github.com>
2019-12-30 22:14:38 -0500

    Use correct install path for CodeCoverage.exe

--- build/InstallCodeCoverage.ps1
+++ build/InstallCodeCoverage.ps1
@@ -4,5 +4,5 @@ nuget install Microsoft.CodeCoverage -Version 16.4.0
 cd ..
 
 New-Item -Type Directory -Force -Path "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Team Tools"
-Move-Item -Path "Microsoft.CodeCoverage.16.4.0\build\netstandard1.0\CodeCoverage" -Destination "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Team Tools\Dynamic Code Coverage Tools"
+Move-Item -Path "Temp\Microsoft.CodeCoverage.16.4.0\build\netstandard1.0\CodeCoverage" -Destination "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Team Tools\Dynamic Code Coverage Tools"
 Remove-Item -Recurse -Force Temp
