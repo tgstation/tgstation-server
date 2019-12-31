3211164dd8179ebcd2fdc06a40f884649bd0c361
Jordan Brown <Cyberboss@users.noreply.github.com>
2019-12-31 09:53:57 -0500

    Try including the CodeCoverage shim dll

--- build/InstallCodeCoverage.ps1
+++ build/InstallCodeCoverage.ps1
@@ -5,4 +5,5 @@ cd ..
 
 New-Item -Type Directory -Force -Path "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Team Tools"
 Move-Item -Path "Temp\Microsoft.CodeCoverage.16.4.0\build\netstandard1.0\CodeCoverage" -Destination "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Team Tools\Dynamic Code Coverage Tools"
+Move-Item -Path "Temp\Microsoft.CodeCoverage.16.4.0\lib\net45\Microsoft.VisualStudio.CodeCoverage.Shim.dll" -Destination "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Team Tools\Dynamic Code Coverage Tools"
 Remove-Item -Recurse -Force Temp
