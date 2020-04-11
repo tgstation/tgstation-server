96c4d3970e3cc7c958e0fd11001f79bbb91098f1
Jordan Brown <Cyberboss@users.noreply.github.com>
2020-04-11 16:14:00 -0400

    Add deployment process for API and DMAPI

--- build/prep_deployment.ps1
+++ build/prep_deployment.ps1
@@ -1,13 +1,16 @@
-$env:TGSVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$env:APPVEYOR_BUILD_FOLDER/artifacts/ServerHost/Tgstation.Server.Host.dll").FileVersion
+$bf = $env:APPVEYOR_BUILD_FOLDER
+[XML]$versionXML = Get-Content "$bf/build/Version.props"
+$env:TGSVersion = $versionXML.Project.PropertyGroup.TgsCoreVersion
+$env:APIVersion = $versionXML.Project.PropertyGroup.TgsApiVersion
+$env:DMVersion = $versionXML.Project.PropertyGroup.TgsDmapiVersion
 
 Write-Host "TGS Version: $env:TGSVersion"
 
 if (($env:CONFIGURATION -match "Release") -And ($env:APPVEYOR_REPO_BRANCH -match "master") -And ($env:APPVEYOR_REPO_COMMIT_MESSAGE -match "\[TGSDeploy\]")) {
-    Write-Host "Deploying..."
+    Write-Host "Deploying TGS..."
     $env:TGSDeploy = "Do it." 
 
     Write-Host "Generating release notes..."
-    $bf = $env:APPVEYOR_BUILD_FOLDER
     dotnet run -p "$bf/tools/ReleaseNotes" $env:TGSVersion
     $env:TGSDraftNotes = !($?)
     $releaseNotesPath = "$bf/release_notes.md"
@@ -18,7 +21,19 @@ if (($env:CONFIGURATION -match "Release") -And ($env:APPVEYOR_REPO_BRANCH -match
     else {
         Write-Host "Release note generation failed, release will be created as a draft!"
         $env:TGSReleaseNotes = "Automatic generation failed, please fill manually!"
-    }      
+    }
+}
+
+if (($env:CONFIGURATION -match "Release")-And ($env:APPVEYOR_REPO_COMMIT_MESSAGE -match "\[APIDeploy\]")) {
+    Write-Host "Deploying API..."
+    $env:APIDeploy = "Do it." 
+    $env:APIReleaseNotes = "# tgstation-server 4 API v$env:APIVersion"
+}
+
+if (($env:CONFIGURATION -match "Release")-And ($env:APPVEYOR_REPO_COMMIT_MESSAGE -match "\[DMDeploy\]")) {
+    Write-Host "Deploying DMAPI..."
+    $env:DMDeploy = "Do it." 
+    $env:DMReleaseNotes = "# tgstation-server 4 DMAPI v$env:DMReleaseNotes"
 }
     
 if (($env:APPVEYOR_REPO_BRANCH -match "master") -And ($env:APPVEYOR_REPO_COMMIT_MESSAGE -match "\[NugetDeploy\]")) {
