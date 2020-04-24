1d1854f8423645899b6ad3df6d059887cf3893b5
Jordan Brown <Cyberboss@users.noreply.github.com>
2020-04-24 14:26:51 -0400

    Add more logging to docker startup

--- build/tgs.docker.sh
+++ build/tgs.docker.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
+$SCRIPT_VERSION="1.1.0"
+
+echo "tgstation-server 4 container startup script v$SCRIPT_VERSION"
+echo "PWD: $PWD"
+
 PROD_CONFIG=/config_data/appsettings.Production.json
+HOST_CONFIG=/app/appsettings.Production.json
+
 if [ ! -f $PROD_CONFIG ]; then
+	echo "$PROD_CONFIG not detected! Creating empty..."
 	echo "{}" > $PROD_CONFIG
 fi
-ln -s $PROD_CONFIG /app/appsettings.Production.json
 
+echo "Linking $PROD_CONFIG to $HOST_CONFIG"
+ln -s $PROD_CONFIG $HOST_CONFIG
+
+echo "Executing console runner..."
 exec dotnet Tgstation.Server.Host.Console.dll
