99b343da0d863f5aeb9f6f6bf34f2585317af2ab
Jordan Brown <Cyberboss@users.noreply.github.com>
2020-04-27 19:44:45 -0400

    Allows joining IRC channels with keys
    - API bump to 6.1.0.
    - Store the key colon separated with the channel.

--- src/Tgstation.Server.Host/Components/Chat/Providers/IrcProvider.cs
+++ src/Tgstation.Server.Host/Components/Chat/Providers/IrcProvider.cs
@@ -9,6 +9,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Tgstation.Server.Api.Models;
 using Tgstation.Server.Host.Core;
+using Tgstation.Server.Host.Extensions;
 using Tgstation.Server.Host.System;
 
 namespace Tgstation.Server.Host.Components.Chat.Providers
@@ -376,24 +377,34 @@ namespace Tgstation.Server.Host.Components.Chat.Providers
 		}
 
 		/// <inheritdoc />
-		public override Task<IReadOnlyCollection<ChannelRepresentation>> MapChannels(IEnumerable<Api.Models.ChatChannel> channels, CancellationToken cancellationToken) => Task.Factory.StartNew(() =>
+		public override Task<IReadOnlyCollection<ChannelRepresentation>> MapChannels(IEnumerable<ChatChannel> channels, CancellationToken cancellationToken) => Task.Factory.StartNew(() =>
 		{
 			if (channels.Any(x => x.IrcChannel == null))
 				throw new InvalidOperationException("ChatChannel missing IrcChannel!");
 			lock (this)
 			{
+				var channelsWithKeys = new Dictionary<string, string>();
 				var hs = new HashSet<string>(); // for unique inserts
-				foreach (var I in channels)
-					hs.Add(I.IrcChannel);
-				var toPart = new List<string>();
-				foreach (var I in client.JoinedChannels)
-					if (!hs.Remove(I))
-						toPart.Add(I);
+				foreach (var channel in channels)
+				{
+					var name = channel.GetIrcChannelName();
+					var key = channel.GetIrcChannelKey();
+					if (hs.Add(name) && key != null)
+						channelsWithKeys.Add(name, key);
+				}
 
-				foreach (var I in toPart)
-					client.RfcPart(I, "Pretty nice abscond!");
-				foreach (var I in hs)
-					client.RfcJoin(I);
+				var toPart = new List<string>();
+				foreach (var activeChannel in client.JoinedChannels)
+					if (!hs.Remove(activeChannel))
+						toPart.Add(activeChannel);
+
+				foreach (var channelToLeave in toPart)
+					client.RfcPart(channelToLeave, "Pretty nice abscond!");
+				foreach (var channelToJoin in hs)
+					if (channelsWithKeys.TryGetValue(channelToJoin, out var key))
+						client.RfcJoin(channelToJoin, key);
+					else
+						client.RfcJoin(channelToJoin);
 
 				return (IReadOnlyCollection<ChannelRepresentation>)channels.Select(x =>
 				{
