0b73908b3b2fd40818825505c8e342df412f426a
Jordan Brown <Cyberboss@users.noreply.github.com>
2020-05-04 01:50:56 -0400

    Remove integration test looping

--- tests/Tgstation.Server.Tests/IntegrationTest.cs
+++ tests/Tgstation.Server.Tests/IntegrationTest.cs
@@ -172,11 +172,6 @@ namespace Tgstation.Server.Tests
 		[TestMethod]
 		public async Task TestFullStandardOperation()
 		{
-			while (true)
-				await TestImpl();
-		}
-
-		async Task TestImpl() { 
 			RequireDiscordToken();
 			var server = new TestingServer(clientFactory, null);
 			using (var serverCts = new CancellationTokenSource())
