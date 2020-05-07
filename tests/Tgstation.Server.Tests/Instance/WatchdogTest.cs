27a3f75e6a5b8481692f53bec4f3276c6bec6175
Jordan Brown <Cyberboss@users.noreply.github.com>
2020-05-06 22:48:16 -0400

    On Windows I suppose this is just a timing issue

--- tests/Tgstation.Server.Tests/Instance/WatchdogTest.cs
+++ tests/Tgstation.Server.Tests/Instance/WatchdogTest.cs
@@ -183,7 +183,7 @@ namespace Tgstation.Server.Tests.Instance
 			var result = await bts.SendTopic(IPAddress.Loopback, "tgs_integration_test_special_tactics=1", 1337, cancellationToken);
 			Assert.AreEqual("ack", result);
 
-			await Task.Delay(6000, cancellationToken);
+			await Task.Delay(7000, cancellationToken);
 		}
 
 		async Task<DreamDaemon> DeployTestDme(string dmeName, DreamDaemonSecurity deploymentSecurity, CancellationToken cancellationToken)
