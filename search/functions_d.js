65d2dc81628e5f51385686243a0f27b15c55a855
tgstation-server <tgstation-server@tgstation13.org>
2020-05-08 18:23:56 +0000

    Deploy code docs to GitHub Pages for Appveyor build 1143
    Commit: d46af666cbbd03dd26bc097f4d6eb9909f525220

--- search/functions_d.js
+++ search/functions_d.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['nextmessage_3333',['NextMessage',['../interface_tgstation_1_1_server_1_1_host_1_1_components_1_1_chat_1_1_providers_1_1_i_provider.html#ab6cd020395b652aa9d79c00497b739fb',1,'Tgstation.Server.Host.Components.Chat.Providers.IProvider.NextMessage()'],['../class_tgstation_1_1_server_1_1_host_1_1_components_1_1_chat_1_1_providers_1_1_provider.html#a962a3510e617d6754d8e21a237d4b933',1,'Tgstation.Server.Host.Components.Chat.Providers.Provider.NextMessage()']]],
   ['normalizeanddelete_3334',['NormalizeAndDelete',['../class_tgstation_1_1_server_1_1_host_1_1_i_o_1_1_default_i_o_manager.html#ab0629dee77100aa23cc5f7934efccdb7',1,'Tgstation::Server::Host::IO::DefaultIOManager']]],
-  ['normalizemention_3335',['NormalizeMention',['../class_tgstation_1_1_server_1_1_host_1_1_components_1_1_chat_1_1_providers_1_1_discord_provider.html#a8618deb5180b94f46015301982fa36a1',1,'Tgstation::Server::Host::Components::Chat::Providers::DiscordProvider']]],
+  ['normalizementions_3335',['NormalizeMentions',['../class_tgstation_1_1_server_1_1_host_1_1_components_1_1_chat_1_1_providers_1_1_discord_provider.html#a3cf5b9ba9079d6c9f582f61b5894f7c1',1,'Tgstation::Server::Host::Components::Chat::Providers::DiscordProvider']]],
   ['normalizepath_3336',['NormalizePath',['../class_tgstation_1_1_server_1_1_host_1_1_controllers_1_1_instance_controller.html#a8b8a858eef2c16d29e1aff3603f91cce',1,'Tgstation::Server::Host::Controllers::InstanceController']]],
   ['notifyoffailedreattach_3337',['NotifyOfFailedReattach',['../class_tgstation_1_1_server_1_1_host_1_1_components_1_1_watchdog_1_1_watchdog_base.html#a8b6f38ba84751a83a7db0a28ccef95ae',1,'Tgstation::Server::Host::Components::Watchdog::WatchdogBase']]]
 ];
