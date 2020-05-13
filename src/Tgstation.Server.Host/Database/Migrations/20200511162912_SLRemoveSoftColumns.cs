f2d50e0d5aa1336253980ecd413afddc287f5d20
Jordan Brown <Cyberboss@users.noreply.github.com>
2020-05-12 23:37:13 -0400

    Fix SQLite migration

--- src/Tgstation.Server.Host/Database/Migrations/20200511162912_SLRemoveSoftColumns.cs
+++ src/Tgstation.Server.Host/Database/Migrations/20200511162912_SLRemoveSoftColumns.cs
@@ -8,7 +8,7 @@ namespace Tgstation.Server.Host.Database.Migrations
 	/// </summary>
 	public partial class SLRemoveSoftColumns : Migration
 	{
-		const string MigratedColumns = "AllowWebClient,SecurityLevel,PrimaryPort,SecondaryPort,AutoStart,InstanceId";
+		const string MigratedColumns = "Id,AllowWebClient,SecurityLevel,PrimaryPort,SecondaryPort,AutoStart,HeartbeatSeconds,StartupTimeout,InstanceId";
 
 		/// <inheritdoc />
 		protected override void Up(MigrationBuilder migrationBuilder)
@@ -20,33 +20,8 @@ namespace Tgstation.Server.Host.Database.Migrations
 				name: "DreamDaemonSettings",
 				newName: "DreamDaemonSettings_up");
 
-			migrationBuilder.CreateTable(
-				name: "DreamDaemonSettings",
-				columns: table => new
-				{
-					Id = table.Column<long>(nullable: false)
-						.Annotation("Sqlite:Autoincrement", true),
-					AllowWebClient = table.Column<bool>(nullable: false),
-					SecurityLevel = table.Column<int>(nullable: false),
-					PrimaryPort = table.Column<ushort>(nullable: false),
-					SecondaryPort = table.Column<ushort>(nullable: false),
-					StartupTimeout = table.Column<uint>(nullable: false),
-					AutoStart = table.Column<bool>(nullable: false),
-					InstanceId = table.Column<long>(nullable: false)
-				},
-				constraints: table =>
-				{
-					table.PrimaryKey("PK_DreamDaemonSettings", x => x.Id);
-					table.ForeignKey(
-						name: "FK_DreamDaemonSettings_Instances_InstanceId",
-						column: x => x.InstanceId,
-						principalTable: "Instances",
-						principalColumn: "Id",
-						onDelete: ReferentialAction.Cascade);
-				});
-
 			migrationBuilder.Sql(
-				$"INSERT INTO DreamDaemonSettings ({MigratedColumns}) VALUES SELECT {MigratedColumns} FROM DreamDaemonSettings_up");
+				$"CREATE TABLE DreamDaemonSettings AS SELECT {MigratedColumns} FROM DreamDaemonSettings_up");
 
 			migrationBuilder.DropTable(
 				name: "DreamDaemonSettings_up");
@@ -58,44 +33,27 @@ namespace Tgstation.Server.Host.Database.Migrations
 			if (migrationBuilder == null)
 				throw new ArgumentNullException(nameof(migrationBuilder));
 
-			migrationBuilder.RenameTable(
-				name: "DreamDaemonSettings",
-				newName: "DreamDaemonSettings_down");
+			migrationBuilder.AddColumn<string>(
+				name: "AccessToken",
+				table: "DreamDaemonSettings",
+				nullable: true);
 
-			migrationBuilder.CreateTable(
-				   name: "DreamDaemonSettings",
-				   columns: table => new
-				   {
-					   Id = table.Column<long>(nullable: false)
-						   .Annotation("Sqlite:Autoincrement", true),
-					   AllowWebClient = table.Column<bool>(nullable: false),
-					   SecurityLevel = table.Column<int>(nullable: false),
-					   PrimaryPort = table.Column<ushort>(nullable: false),
-					   SecondaryPort = table.Column<ushort>(nullable: false),
-					   StartupTimeout = table.Column<uint>(nullable: false),
-					   AutoStart = table.Column<bool>(nullable: false),
-					   SoftRestart = table.Column<bool>(nullable: false, defaultValue: false),
-					   SoftShutdown = table.Column<bool>(nullable: false, defaultValue: false),
-					   ProcessId = table.Column<int>(nullable: true, defaultValue: 0),
-					   AccessToken = table.Column<string>(nullable: true),
-					   InstanceId = table.Column<long>(nullable: false)
-				   },
-				   constraints: table =>
-				   {
-					   table.PrimaryKey("PK_DreamDaemonSettings", x => x.Id);
-					   table.ForeignKey(
-						   name: "FK_DreamDaemonSettings_Instances_InstanceId",
-						   column: x => x.InstanceId,
-						   principalTable: "Instances",
-						   principalColumn: "Id",
-						   onDelete: ReferentialAction.Cascade);
-				   });
+			migrationBuilder.AddColumn<int>(
+				name: "ProcessId",
+				table: "DreamDaemonSettings",
+				nullable: true);
 
-			migrationBuilder.Sql(
-				$"INSERT INTO DreamDaemonSettings ({MigratedColumns}) VALUES SELECT {MigratedColumns} FROM DreamDaemonSettings_down");
+			migrationBuilder.AddColumn<bool>(
+				name: "SoftRestart",
+				table: "DreamDaemonSettings",
+				nullable: false,
+				defaultValue: false);
 
-			migrationBuilder.DropTable(
-				name: "DreamDaemonSettings_down");
+			migrationBuilder.AddColumn<bool>(
+				name: "SoftShutdown",
+				table: "DreamDaemonSettings",
+				nullable: false,
+				defaultValue: false);
 		}
 	}
 }
