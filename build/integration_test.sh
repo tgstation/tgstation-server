e3d863783c4baabd4125e795d6d7e5b18ea19576
Jordan Brown <Cyberboss@users.noreply.github.com>
2020-05-16 12:15:33 -0400

    Fixes

--- build/integration_test.sh
+++ build/integration_test.sh
@@ -6,7 +6,11 @@ export TGS4_TEST_IRC_CHANNEL=\#botbus
 export TGS4_TEST_TEMP_DIRECTORY=~/tgs4_test
 #token set in CI settings
 
-$HOME/.dotnet/tools/coverlet bin/$CONFIG/netcoreapp3.1/Tgstation.Server.Tests.dll --target "dotnet" --targetargs "test -c $CONFIG" --format opencover --output "../../TestResults/integration_test.xml" --include "[Tgstation.Server*]*" --exclude "[Tgstation.Server.Tests*]*" --exclude "[Tgstation.Server.Host]Tgstation.Server.Host.Database.Migrations.*"
+cd tests/Tgstation.Server.Tests
+
+dotnet build -c $CONFIG
+
+$HOME/.dotnet/tools/coverlet bin/$CONFIG/netcoreapp3.1/Tgstation.Server.Tests.dll --target "dotnet" --targetargs "test -c $CONFIG --no-build" --format opencover --output "../../TestResults/integration_test.xml" --include "[Tgstation.Server*]*" --exclude "[Tgstation.Server.Tests*]*" --exclude "[Tgstation.Server.Host]Tgstation.Server.Host.Database.Migrations.*"
 
 cd ../../TestResults
 
