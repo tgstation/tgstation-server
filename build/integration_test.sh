7d9ee9ee216763a4fb1c0e1fad86086b2f30122c
Jordan Brown <Cyberboss@users.noreply.github.com>
2020-06-13 16:37:21 -0400

    TRRRRAAAAAAAAAAAAAAAVVVVVVVVVVVIIIIIIIIIIISSSSSS!!

--- build/integration_test.sh
+++ build/integration_test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Needed so gcore can work
+echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+
 export TGS4_TEST_DISCORD_CHANNEL=493119635319947269
 export TGS4_TEST_IRC_CHANNEL=\#botbus
 export TGS4_TEST_TEMP_DIRECTORY=~/tgs4_test
