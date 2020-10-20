8b189d9fd37cd86c91981d2e9eded3ef75f89426
Jordan Brown <Cyberboss@users.noreply.github.com>
2020-10-20 13:36:09 -0400

    Minor integration test timeout buff [DMDeploy]

--- tests/Tgstation.Server.Tests/IntegrationTest.cs
+++ tests/Tgstation.Server.Tests/IntegrationTest.cs
@@ -361,7 +361,7 @@ namespace Tgstation.Server.Tests
 					foreach (var job in jobs)
 					{
 						Assert.IsTrue(job.StartedAt.Value >= preStartupTime);
-						await jrt.WaitForJob(job, 120, false, null, cancellationToken);
+						await jrt.WaitForJob(job, 140, false, null, cancellationToken);
 					}
 
 					var dd = await instanceClient.DreamDaemon.Read(cancellationToken);
