723d2d9a6e22fe8f007346694e18077130abdc63
Jordan Brown <Cyberboss@users.noreply.github.com>
2020-12-09 16:34:19 -0500

    Fix SQLite down migration
    Wish were on .NET 5 for that sweet sweet DropColumn support

--- src/Tgstation.Server.Host/Database/Migrations/20201209194554_SLGenericTestMergingUpdate.cs
+++ src/Tgstation.Server.Host/Database/Migrations/20201209194554_SLGenericTestMergingUpdate.cs
@@ -36,9 +36,50 @@ namespace Tgstation.Server.Host.Database.Migrations
 				table: "TestMerges",
 				newName: "PullRequestRevision");
 
-			migrationBuilder.DropColumn(
-				name: "RepositoryOrigin",
-				table: "CompileJobs");
+			migrationBuilder.RenameTable(
+				name: "CompileJobs",
+				newName: "CompileJobs_down");
+
+			migrationBuilder.CreateTable(
+				name: "CompileJobs",
+				columns: table => new
+				{
+					Id = table.Column<long>(nullable: false)
+						.Annotation("Sqlite:Autoincrement", true),
+					DmeName = table.Column<string>(nullable: false),
+					Output = table.Column<string>(nullable: false),
+					DirectoryName = table.Column<Guid>(nullable: false),
+					MinimumSecurityLevel = table.Column<int>(nullable: true),
+					JobId = table.Column<long>(nullable: false),
+					RevisionInformationId = table.Column<long>(nullable: false),
+					ByondVersion = table.Column<string>(nullable: false),
+					DMApiMajorVersion = table.Column<int>(nullable: true),
+					DMApiMinorVersion = table.Column<int>(nullable: true),
+					DMApiPatchVersion = table.Column<int>(nullable: true),
+					GitHubDeploymentId = table.Column<long>(nullable: true),
+					GitHubRepoId = table.Column<long>(nullable: true)
+				},
+				constraints: table =>
+				{
+					table.PrimaryKey("PK_CompileJobs", x => x.Id);
+					table.ForeignKey(
+						name: "FK_CompileJobs_Jobs_JobId",
+						column: x => x.JobId,
+						principalTable: "Jobs",
+						principalColumn: "Id",
+						onDelete: ReferentialAction.Cascade);
+					table.ForeignKey(
+						name: "FK_CompileJobs_RevisionInformations_RevisionInformationId",
+						column: x => x.RevisionInformationId,
+						principalTable: "RevisionInformations",
+						principalColumn: "Id");
+				});
+
+			migrationBuilder.Sql(
+				$"INSERT INTO CompileJobs SELECT Id,DmeName,Output,DirectoryName,MinimumSecurityLevel,JobId,RevisionInformationId,ByondVersion,DMApiMajorVersion,DMApiMinorVersion,DMApiPatchVersion,GitHubDeploymentId,GitHubRepoId FROM CompileJobs_down");
+
+			migrationBuilder.DropTable(
+				name: "CompileJobs_down");
 		}
 	}
 }
