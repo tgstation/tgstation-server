1058c9662774f953613b8d9f61b9028154d359a3
Jordan Brown <Cyberboss@users.noreply.github.com>
2020-12-18 00:33:39 -0500

    Finish using up deprecated ErrorCodes

--- src/Tgstation.Server.Api/Models/ErrorCode.cs
+++ src/Tgstation.Server.Api/Models/ErrorCode.cs
@@ -286,11 +286,10 @@ namespace Tgstation.Server.Api.Models
 		ChatBotProviderMissing,
 
 		/// <summary>
-		/// Attempted to update a <see cref="User"/> or <see cref="InstancePermissionSet"/> without its ID.
+		/// Tried to edit <see cref="UserGroup"/> membership using <see cref="Routes.UserGroup"/>.
 		/// </summary>
-		[Description("Missing user ID!")]
-		[Obsolete("Deprecated in favor of code 2", true)]
-		UserMissingId,
+		[Description("The " + Routes.UserGroup + " endpoint cannot edit group members. Please update each member user individually.")]
+		UserGroupControllerCantEditMembers,
 
 		/// <summary>
 		/// Attempted to add a <see cref="ChatBot"/> when at or above the <see cref="Instance.ChatBotLimit"/> or it was set to something lower than the existing amount of <see cref="ChatBot"/>.
@@ -335,11 +334,10 @@ namespace Tgstation.Server.Api.Models
 		DreamMakerInvalidValidation,
 
 		/// <summary>
-		/// DMAPI validation timeout.
+		/// Tried to remove the last <see cref="OAuthConnection"/> for a passwordless <see cref="User"/>.
 		/// </summary>
-		[Description("The DreamDaemon startup timeout was hit before the DMAPI validated!")]
-		[Obsolete("Deprecated in favor of error code 52", true)]
-		DreamMakerValidationTimeout,
+		[Description("This user is passwordless and removing their oAuthConnections would leave them with no authentication method!")]
+		CannotRemoveLastAuthenticationOption,
 
 		/// <summary>
 		/// No .dme could be found for deployment.
@@ -610,17 +608,5 @@ namespace Tgstation.Server.Api.Models
 		/// </summary>
 		[Description("Cannot delete the user group as it is not empty!")]
 		UserGroupNotEmpty,
-
-		/// <summary>
-		/// Tried to edit <see cref="UserGroup"/> membership using <see cref="Routes.UserGroup"/>.
-		/// </summary>
-		[Description("The " + Routes.UserGroup + " endpoint cannot edit group members. Please update each member user individually.")]
-		UserGroupControllerCantEditMembers,
-
-		/// <summary>
-		/// Tried to remove the last <see cref="OAuthConnection"/> for a passwordless <see cref="User"/>.
-		/// </summary>
-		[Description("This user is passwordless and removing their oAuthConnections would leave them with no authentication method!")]
-		CannotRemoveLastAuthenticationOption,
 	}
 }
