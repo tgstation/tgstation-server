1959257dec2dc8f23eab37c0f4e5d9d5d9ee2186
tgstation-server <tgstation-server@tgstation13.org>
2020-12-20 01:26:48 +0000

    Deploy code docs to GitHub Pages for workflow run 435
    Commit: 61d504a9e45e7d7fc174cb5df0733ce36d571de0

--- search/properties_14.js
+++ search/properties_14.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['watchdog_5378',['Watchdog',['../interface_tgstation_1_1_server_1_1_host_1_1_components_1_1_i_instance_core.html#a71590b8427369b3eb57af54643068448',1,'Tgstation.Server.Host.Components.IInstanceCore.Watchdog()'],['../class_tgstation_1_1_server_1_1_host_1_1_components_1_1_instance.html#a902e9a3917553cae8ec372948e98d6ec',1,'Tgstation.Server.Host.Components.Instance.Watchdog()']]],
   ['watchdogpresent_5379',['WatchdogPresent',['../interface_tgstation_1_1_server_1_1_host_1_1_core_1_1_i_server_control.html#a1c9a042a42da1e39316d2f1b45b28765',1,'Tgstation::Server::Host::Core::IServerControl']]],
-  ['windowshost_5380',['WindowsHost',['../class_tgstation_1_1_server_1_1_api_1_1_models_1_1_administration.html#abf3f89fee1f079cadd4208f337444fec',1,'Tgstation::Server::Api::Models::Administration']]]
+  ['windowshost_5380',['WindowsHost',['../class_tgstation_1_1_server_1_1_api_1_1_models_1_1_server_information.html#a4facf3d6b4b97e90222f3e04b3eefac2',1,'Tgstation::Server::Api::Models::ServerInformation']]]
 ];
