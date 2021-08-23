aea9c8e7b0c8a867363fdfc16072393ba292ce54
Jordan Brown <Cyberboss@users.noreply.github.com>
2021-08-22 22:04:42 -0400

    Switch from Discord.Net to Remora.Discord
    - Remove typing event support.
    - Remove Message.Context as it was used only for that
    - .ToAsyncEnumerable() seemed to have relied on a sub-library of Discord.Net. Removed it's usage in DefaultIOManager.

---
+++ src/Tgstation.Server.Host/Components/Chat/Providers/DiscordForwardingResponder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Remora.Discord.API.Abstractions.Gateway.Events;
+using Remora.Discord.Gateway.Responders;
+using Remora.Results;
+
+namespace Tgstation.Server.Host.Components.Chat.Providers
+{
+	/// <summary>
+	/// An <see cref="IResponder{TGatewayEvent}"/> that forwards to another <see cref="targetResponder"/>.
+	/// </summary>
+	sealed class DiscordForwardingResponder : IDiscordResponders
+	{
+		/// <summary>
+		/// The <see cref="IResponder{TGatewayEvent}"/> to forward the event to.
+		/// </summary>
+		readonly IDiscordResponders targetResponder;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DiscordForwardingResponder"/> class.
+		/// </summary>
+		/// <param name="targetResponder">The value of <see cref="targetResponder"/>.</param>
+		public DiscordForwardingResponder(IDiscordResponders targetResponder)
+		{
+			this.targetResponder = targetResponder ?? throw new ArgumentNullException(nameof(targetResponder));
+		}
+
+		/// <inheritdoc />
+		public Task<Result> RespondAsync(IMessageCreate gatewayEvent, CancellationToken ct) => targetResponder.RespondAsync(gatewayEvent, ct);
+	}
+}
