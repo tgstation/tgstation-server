d0706d7a134fc06e9231a997da76c5fcc2d3381d
Jordan Brown <Cyberboss@users.noreply.github.com>
2021-10-27 13:25:08 -0400

    Backwards compatibility

--- src/Tgstation.Server.Host/Configuration/SessionConfiguration.cs
+++ src/Tgstation.Server.Host/Configuration/SessionConfiguration.cs
@@ -11,9 +11,14 @@
 		public const string Section = "Session";
 
 		/// <summary>
+		/// The default value for <see cref="HighPriorityLiveDreamDaemon"/>.
+		/// </summary>
+		private const bool DefaultHighPriorityLiveDreamDaemon = true;
+
+		/// <summary>
 		/// If the public DreamDaemon instances are set to be above normal priority processes.
 		/// </summary>
-		public bool HighPriorityLiveDreamDaemon { get; set; }
+		public bool HighPriorityLiveDreamDaemon { get; set; } = DefaultHighPriorityLiveDreamDaemon;
 
 		/// <summary>
 		/// If the deployment DreamMaker and DreamDaemon instances are set to be below normal priority processes.
