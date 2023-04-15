19222c9d65e8eba3b81df1beb767f08fefe2f06d
Dominion <Cyberboss@users.noreply.github.com>
2023-04-15 15:12:06 -0400

    Revert "Remove SafeHandle as referencing it does nothing"
    This reverts commit cafc29b51333b301789b2e8e7bb2e2442669f7a2.

--- src/Tgstation.Server.Host/System/Process.cs
+++ src/Tgstation.Server.Host/System/Process.cs
@@ -43,6 +43,12 @@ namespace Tgstation.Server.Host.System
 		readonly CancellationTokenSource readerCts;
 
 		/// <summary>
+		/// The <see cref="global::System.Diagnostics.Process.SafeHandle"/>.
+		/// </summary>
+		/// <remarks>We keep this to prevent .NET from closing the real handle too soon. See https://stackoverflow.com/a/47656845</remarks>
+		readonly SafeProcessHandle safeHandle;
+
+		/// <summary>
 		/// The <see cref="Task{TResult}"/> resulting in the process' standard output/error text.
 		/// </summary>
 		readonly Task<string> readTask;
@@ -69,6 +75,7 @@ namespace Tgstation.Server.Host.System
 			this.handle = handle ?? throw new ArgumentNullException(nameof(handle));
 
 			// Do this fast because the runtime will bitch if we try to access it after it ends
+			safeHandle = handle.SafeHandle;
 			Id = handle.Id;
 
 			this.readerCts = readerCts;
@@ -115,6 +122,7 @@ namespace Tgstation.Server.Host.System
 			if (readTask != null)
 				await readTask;
 
+			safeHandle.Dispose();
 			handle.Dispose();
 		}
 
