b9a077345785667d547f03831284c2193a10fda6
Jordan <Cyberboss@users.noreply.github.com>
2023-06-19 10:37:42 -0400

    Remove worktrees technique from .deb build

--- build/package/deb/build_package.sh
+++ build/package/deb/build_package.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-# Run from git root, assumes dotnet-sdk-6.0 is installed
+# Run from git root, assumes dotnet-sdk-6.0 and npm are installed
+
+set -e
+set -x
 
 dpkg --add-architecture i386
 apt-get update
 apt-get install -y build-essential binutils lintian debhelper dh-make devscripts xmlstarlet # needs cleanup probably, SO copypasta
-
-CURRENT_COMMIT=$(git rev-parse HEAD)
-
-rm -rf packaging
-git worktree remove packaging
-git worktree add -f packaging $CURRENT_COMMIT
-cd packaging
-rm -f .git
 
 export DEBEMAIL="Cyberboss@users.noreply.github.com"
 export DEBFULLNAME="Jordan Dominion"
@@ -24,5 +19,3 @@ cp build/package/deb/debian/* debian/
 cp build/tgstation-server.service debian/
 
 dpkg-buildpackage
-
-cd ..
