56a4a95d6b7d9ac5fbb0dfb0f3e6637441538e8b
Jordan <Cyberboss@users.noreply.github.com>
2023-06-19 12:05:39 -0400

    Revert "Remove worktrees technique from .deb build"
    This reverts commit b9a077345785667d547f03831284c2193a10fda6.

--- build/package/deb/build_package.sh
+++ build/package/deb/build_package.sh
@@ -8,6 +8,14 @@ dpkg --add-architecture i386
 apt-get update
 apt-get install -y npm dotnet-sdk-6.0 build-essential binutils lintian debhelper dh-make devscripts xmlstarlet # needs cleanup probably, SO copypasta
 
+CURRENT_COMMIT=$(git rev-parse HEAD)
+
+rm -rf packaging
+git worktree remove packaging
+git worktree add -f packaging $CURRENT_COMMIT
+cd packaging
+rm -f .git
+
 export DEBEMAIL="Cyberboss@users.noreply.github.com"
 export DEBFULLNAME="Jordan Dominion"
 
@@ -19,3 +27,5 @@ cp build/package/deb/debian/* debian/
 cp build/tgstation-server.service debian/
 
 dpkg-buildpackage
+
+cd ..
