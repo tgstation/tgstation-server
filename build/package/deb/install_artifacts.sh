3defb4945016d457810bf01362bef157d35a32d4
Jordan Dominion <Cyberboss@users.noreply.github.com>
2023-06-23 14:51:41 -0400

    `find` only files

--- build/package/deb/install_artifacts.sh
+++ build/package/deb/install_artifacts.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh -e
 
-cd artifacts && for f in $(find *); do install "$f" "$1/opt/tgstation-server/$f"; done
+cd artifacts && for f in $(find * -type f); do install "$f" "$1/opt/tgstation-server/$f"; done
