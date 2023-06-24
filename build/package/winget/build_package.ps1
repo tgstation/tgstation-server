e5858d02f4e90c3cbbbd26f1c64e38d1f8a25486
Jordan Dominion <Cyberboss@users.noreply.github.com>
2023-06-24 09:57:51 -0400

    Enable (roughly) upgrading the installation

--- build/package/winget/build_package.ps1
+++ build/package/winget/build_package.ps1
@@ -13,11 +13,15 @@ Remove-Item -Recurse -Force .git
 [XML]$versionXML = Get-Content build/Version.props
 $tgsVersion = $versionXML.Project.PropertyGroup.TgsCoreVersion
 
-(Get-Content build/package/winget/Tgstation.Server.Host.Service.Msi/Tgstation.Server.Host.Service.Msi.vdproj).Replace('22.47.5', $tgsVersion) | Set-Content build/package/winget/Tgstation.Server.Host.Service.Msi/Tgstation.Server.Host.Service.Msi.vdproj
+$devProductCode = 'D24887FA-3228-4509-B5F3-4E07E349F278'
+$devVersion = '0.22.475'
+
+(Get-Content build/package/winget/Tgstation.Server.Host.Service.Msi/Tgstation.Server.Host.Service.Msi.vdproj).Replace($devVersion, $tgsVersion) | Set-Content build/package/winget/Tgstation.Server.Host.Service.Msi/Tgstation.Server.Host.Service.Msi.vdproj
+(Get-Content build/package/winget/Tgstation.Server.Host.Service.Msi/Tgstation.Server.Host.Service.Msi.vdproj).Replace($devProductCode, [guid]::NewGuid().ToString().ToUpperInvariant()) | Set-Content build/package/winget/Tgstation.Server.Host.Service.Msi/Tgstation.Server.Host.Service.Msi.vdproj
 
 dotnet restore
 cd build/package/winget
-dotnet build -c Release Tgstation.Server.Host.Service.Configure/Tgstation.Server.Host.Service.Configure
+dotnet build -c Release Tgstation.Server.Host.Service.Configure
 
 # We make _some_ assumptions
 DevEnv installer.sln /Project Tgstation.Server.Host.Service.Msi/Tgstation.Server.Host.Service.Msi.vdproj /build Release
