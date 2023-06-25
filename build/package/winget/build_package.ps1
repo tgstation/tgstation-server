2760060969f89cfb9f609403550f49235fddc1c6
Jordan Dominion <Cyberboss@users.noreply.github.com>
2023-06-25 12:24:56 -0400

    dotnet commands here do not speed up the build

--- build/package/winget/build_package.ps1
+++ build/package/winget/build_package.ps1
@@ -13,15 +13,13 @@ Remove-Item -Recurse -Force .git
 [XML]$versionXML = Get-Content build/Version.props
 $tgsVersion = $versionXML.Project.PropertyGroup.TgsCoreVersion
 
+cd build/package/winget
+
 $devProductCode = 'D24887FA-3228-4509-B5F3-4E07E349F278'
 $devVersion = '0.22.475'
 
-(Get-Content build/package/winget/Tgstation.Server.Host.Service.Msi/Tgstation.Server.Host.Service.Msi.vdproj).Replace($devVersion, $tgsVersion) | Set-Content build/package/winget/Tgstation.Server.Host.Service.Msi/Tgstation.Server.Host.Service.Msi.vdproj
-(Get-Content build/package/winget/Tgstation.Server.Host.Service.Msi/Tgstation.Server.Host.Service.Msi.vdproj).Replace($devProductCode, [guid]::NewGuid().ToString().ToUpperInvariant()) | Set-Content build/package/winget/Tgstation.Server.Host.Service.Msi/Tgstation.Server.Host.Service.Msi.vdproj
-
-dotnet restore
-cd build/package/winget
-dotnet build -c Release Tgstation.Server.Host.Service.Configure
+(Get-Content Tgstation.Server.Host.Service.Msi/Tgstation.Server.Host.Service.Msi.vdproj).Replace($devVersion, $tgsVersion) | Set-Content Tgstation.Server.Host.Service.Msi/Tgstation.Server.Host.Service.Msi.vdproj
+(Get-Content Tgstation.Server.Host.Service.Msi/Tgstation.Server.Host.Service.Msi.vdproj).Replace($devProductCode, [guid]::NewGuid().ToString().ToUpperInvariant()) | Set-Content Tgstation.Server.Host.Service.Msi/Tgstation.Server.Host.Service.Msi.vdproj
 
 # We make _some_ assumptions
 DevEnv installer.sln /Project Tgstation.Server.Host.Service.Msi/Tgstation.Server.Host.Service.Msi.vdproj /build Release
