3861d2db8d2585e940335a38db9743f816829fcd
Jordan Dominion <Cyberboss@users.noreply.github.com>
2023-06-25 20:59:34 -0400

    Disallow msi upgrades
    See https://stackoverflow.com/a/14736573
    
    Also set some additional .vdproj fields

--- build/package/winget/build_package.ps1
+++ build/package/winget/build_package.ps1
@@ -17,11 +17,9 @@ $tgsVersion = $versionXML.Project.PropertyGroup.TgsCoreVersion
 
 cd build/package/winget
 
-$devProductCode = 'D24887FA-3228-4509-B5F3-4E07E349F278'
 $devVersion = '0.22.475'
 
 (Get-Content Tgstation.Server.Host.Service.Msi/Tgstation.Server.Host.Service.Msi.vdproj).Replace($devVersion, $tgsVersion) | Set-Content Tgstation.Server.Host.Service.Msi/Tgstation.Server.Host.Service.Msi.vdproj
-(Get-Content Tgstation.Server.Host.Service.Msi/Tgstation.Server.Host.Service.Msi.vdproj).Replace($devProductCode, [guid]::NewGuid().ToString().ToUpperInvariant()) | Set-Content Tgstation.Server.Host.Service.Msi/Tgstation.Server.Host.Service.Msi.vdproj
 
 # We make _some_ assumptions
 DevEnv installer.sln /Project Tgstation.Server.Host.Service.Msi/Tgstation.Server.Host.Service.Msi.vdproj /build Release
