5b468ca0116ac4d91939f5c1ca5e1a8e4c0d5881
Jordan Dominion <Cyberboss@users.noreply.github.com>
2023-10-09 16:15:00 -0400

    Move DotnetHelper to Host Common lib
    Done squashing commits

---
+++ src/Tgstation.Server.Host.Common/DotnetHelper.cs
@@ -0,0 +1,54 @@
+﻿#if NET6_0_OR_GREATER
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+#endif
+
+namespace Tgstation.Server.Host.Common
+{
+#if NET6_0_OR_GREATER
+	/// <summary>
+	/// Helper functions for working with dotnet.exe.
+	/// </summary>
+	public static class DotnetHelper
+	{
+		/// <summary>
+		/// Gets the path to the dotnet executable.
+		/// </summary>
+		/// <param name="isWindows">If the current system is a Windows OS.</param>
+		/// <returns>The path to the dotnet executable.</returns>
+		public static IEnumerable<string> GetPotentialDotnetPaths(bool isWindows)
+		{
+			var enviromentPath = Environment.GetEnvironmentVariable("PATH");
+			var paths = enviromentPath.Split(';');
+
+			var exeName = "dotnet";
+			IEnumerable<string> enumerator;
+			if (isWindows)
+			{
+				exeName += ".exe";
+				enumerator = new List<string>(paths)
+				{
+					"C:/Program Files/dotnet",
+					"C:/Program Files (x86)/dotnet",
+				};
+			}
+			else
+				enumerator = paths
+					.Select(x => x.Split(':'))
+					.SelectMany(x => x)
+					.Concat(new List<string>(2)
+					{
+						"/usr/bin",
+						"/usr/share/bin",
+						"/usr/local/share/dotnet",
+					});
+
+			enumerator = enumerator.Select(x => Path.Combine(x, exeName));
+
+			return enumerator;
+		}
+	}
+#endif
+}
