c007c24a91b412917128cfbd268c3e34e68f8994
Jordan Dominion <Cyberboss@users.noreply.github.com>
2023-11-23 19:29:03 -0500

    Nullify `IApiHeadersProvider`

--- src/Tgstation.Server.Host/Utils/IApiHeadersProvider.cs
+++ src/Tgstation.Server.Host/Utils/IApiHeadersProvider.cs
@@ -1,7 +1,5 @@
 ﻿using Tgstation.Server.Api;
 
-#nullable disable
-
 namespace Tgstation.Server.Host.Utils
 {
 	/// <summary>
@@ -12,12 +10,12 @@ namespace Tgstation.Server.Host.Utils
 		/// <summary>
 		/// The created <see cref="Api.ApiHeaders"/>, if any.
 		/// </summary>
-		ApiHeaders ApiHeaders { get; }
+		ApiHeaders? ApiHeaders { get; }
 
 		/// <summary>
 		/// The <see cref="Api.HeadersException"/> thrown when attempting to parse the <see cref="ApiHeaders"/> if any.
 		/// </summary>
-		HeadersException HeadersException { get; }
+		HeadersException? HeadersException { get; }
 
 		/// <summary>
 		/// Attempt to create <see cref="Api.ApiHeaders"/> without checking for the presence of an <see cref="Microsoft.Net.Http.Headers.HeaderNames.Authorization"/> header.
