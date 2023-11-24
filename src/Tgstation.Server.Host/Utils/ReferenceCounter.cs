55acf36edf022691d2ddf4747713a12c0b95dd59
Jordan Dominion <Cyberboss@users.noreply.github.com>
2023-11-23 19:34:59 -0500

    Nullify `ReferenceCounter`

--- src/Tgstation.Server.Host/Utils/ReferenceCounter.cs
+++ src/Tgstation.Server.Host/Utils/ReferenceCounter.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 
-#nullable disable
-
 namespace Tgstation.Server.Host.Utils
 {
 	/// <summary>
@@ -24,12 +22,12 @@ namespace Tgstation.Server.Host.Utils
 		/// <summary>
 		/// Backing field for <see cref="Instance"/>.
 		/// </summary>
-		TInstance actualInstance;
+		TInstance? actualInstance;
 
 		/// <summary>
 		/// The <see cref="Action"/> to take when <see cref="Dispose"/> is called.
 		/// </summary>
-		Action referenceCleanupAction;
+		Action? referenceCleanupAction;
 
 		/// <summary>
 		/// If the <see cref="ReferenceCounter{TInstance}"/> was initialized.
