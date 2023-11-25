65f26d7d312ab874479b96641dd69ff4a7a90a17
Jordan Dominion <Cyberboss@users.noreply.github.com>
2023-11-25 16:07:10 -0500

    Nullify `PostSetupServices`

--- src/Tgstation.Server.Host/Setup/PostSetupServices.cs
+++ src/Tgstation.Server.Host/Setup/PostSetupServices.cs
@@ -6,8 +6,6 @@ using Microsoft.Extensions.Options;
 using Tgstation.Server.Host.Configuration;
 using Tgstation.Server.Host.System;
 
-#nullable disable
-
 namespace Tgstation.Server.Host.Setup
 {
 	/// <inheritdoc />
