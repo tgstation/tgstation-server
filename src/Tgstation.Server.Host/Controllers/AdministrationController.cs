1d8959ddcd88f20b5ce8f03f75c36a3052aa8a79
Jordan Dominion <Cyberboss@users.noreply.github.com>
2023-11-25 16:18:37 -0500

    Nullify `AdministrationController`

--- src/Tgstation.Server.Host/Controllers/AdministrationController.cs
+++ src/Tgstation.Server.Host/Controllers/AdministrationController.cs
@@ -29,8 +29,6 @@ using Tgstation.Server.Host.Transfer;
 using Tgstation.Server.Host.Utils;
 using Tgstation.Server.Host.Utils.GitHub;
 
-#nullable disable
-
 namespace Tgstation.Server.Host.Controllers
 {
 	/// <summary>
@@ -146,8 +144,8 @@ namespace Tgstation.Server.Host.Controllers
 		{
 			try
 			{
-				Version greatestVersion = null;
-				Uri repoUrl = null;
+				Version? greatestVersion = null;
+				Uri? repoUrl = null;
 				try
 				{
 					var gitHubService = gitHubServiceFactory.CreateService();
@@ -215,10 +213,10 @@ namespace Tgstation.Server.Host.Controllers
 			var attemptingUpload = model.UploadZip == true;
 			if (attemptingUpload)
 			{
-				if (!AuthenticationContext.PermissionSet.AdministrationRights.Value.HasFlag(AdministrationRights.UploadVersion))
+				if (!AuthenticationContext.PermissionSet.AdministrationRights!.Value.HasFlag(AdministrationRights.UploadVersion))
 					return Forbid();
 			}
-			else if (!AuthenticationContext.PermissionSet.AdministrationRights.Value.HasFlag(AdministrationRights.ChangeVersion))
+			else if (!AuthenticationContext.PermissionSet.AdministrationRights!.Value.HasFlag(AdministrationRights.ChangeVersion))
 				return Forbid();
 
 			if (model.NewVersion == null)
@@ -379,7 +377,7 @@ namespace Tgstation.Server.Host.Controllers
 		/// <returns>A <see cref="ValueTask{TResult}"/> resulting in the <see cref="IActionResult"/> of the request.</returns>
 		async ValueTask<IActionResult> AttemptInitiateUpdate(Version newVersion, bool attemptingUpload, CancellationToken cancellationToken)
 		{
-			IFileUploadTicket uploadTicket = attemptingUpload
+			IFileUploadTicket? uploadTicket = attemptingUpload
 				? fileTransferService.CreateUpload(FileUploadStreamKind.None)
 				: null;
 
@@ -393,7 +391,7 @@ namespace Tgstation.Server.Host.Controllers
 				catch
 				{
 					if (attemptingUpload)
-						await uploadTicket.DisposeAsync();
+						await uploadTicket!.DisposeAsync();
 
 					throw;
 				}
