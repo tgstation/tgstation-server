0e209b933fc9a078902293a47edf530fc2e092bf
Jordan Dominion <Cyberboss@users.noreply.github.com>
2023-12-16 11:34:04 -0500

    Essentially remove live tests token expiry

--- tests/Tgstation.Server.Tests/Live/LiveTestingServer.cs
+++ tests/Tgstation.Server.Tests/Live/LiveTestingServer.cs
@@ -153,6 +153,7 @@ namespace Tgstation.Server.Tests.Live
 				$"Session:LowPriorityDeploymentProcesses={LowPriorityDeployments}",
 				$"General:SkipAddingByondFirewallException={!TestingUtils.RunningInGitHubActions}",
 				$"General:OpenDreamGitUrl={OpenDreamUrl}",
+				$"Security:TokenExpiryMinutes=120", // timeouts are useless for us
 			};
 
 			swarmArgs = new List<string>();
