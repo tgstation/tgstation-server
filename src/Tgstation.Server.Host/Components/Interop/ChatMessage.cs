617dea50c3e420c987d24ac0efad82747226bbd1
Jordan Dominion <Cyberboss@users.noreply.github.com>
2023-12-23 10:15:56 -0500

    Nullify `ChatMessage`

--- src/Tgstation.Server.Host/Components/Interop/ChatMessage.cs
+++ src/Tgstation.Server.Host/Components/Interop/ChatMessage.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
 
-#nullable disable
-
 namespace Tgstation.Server.Host.Components.Interop
 {
 	/// <summary>
@@ -12,6 +10,6 @@ namespace Tgstation.Server.Host.Components.Interop
 		/// <summary>
 		/// The <see cref="ICollection{T}"/> of <see cref="Chat.ChannelRepresentation.Id"/>s to sent the <see cref="MessageContent"/> to. Must be safe to parse as <see cref="ulong"/>s.
 		/// </summary>
-		public ICollection<string> ChannelIds { get; set; }
+		public ICollection<string>? ChannelIds { get; set; }
 	}
 }
