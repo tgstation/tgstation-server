0b3b41dca70a4d8e996a71aa634b4b17cbe43bbd
Jordan Dominion <Cyberboss@users.noreply.github.com>
2023-12-23 11:44:15 -0500

    Nullify `TestMergeResult`

--- src/Tgstation.Server.Host/Components/Repository/TestMergeResult.cs
+++ src/Tgstation.Server.Host/Components/Repository/TestMergeResult.cs
@@ -2,8 +2,6 @@
 
 using LibGit2Sharp;
 
-#nullable disable
-
 namespace Tgstation.Server.Host.Components.Repository
 {
 	/// <summary>
@@ -19,6 +17,6 @@ namespace Tgstation.Server.Host.Components.Repository
 		/// <summary>
 		/// List of conflicting file paths relative to the repository root. Only present if <see cref="Status"/> is <see cref="MergeStatus.Conflicts"/>.
 		/// </summary>
-		public IReadOnlyList<string> ConflictingFiles { get; init; }
+		public IReadOnlyList<string>? ConflictingFiles { get; init; }
 	}
 }
