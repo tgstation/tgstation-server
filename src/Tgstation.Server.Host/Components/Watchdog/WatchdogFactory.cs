418b627b44970f633bba00a5d7c531fd04206021
Jordan Dominion <Cyberboss@users.noreply.github.com>
2023-12-23 23:39:29 -0500

    Nullify `WatchdogFactory`

--- src/Tgstation.Server.Host/Components/Watchdog/WatchdogFactory.cs
+++ src/Tgstation.Server.Host/Components/Watchdog/WatchdogFactory.cs
@@ -15,8 +15,6 @@ using Tgstation.Server.Host.IO;
 using Tgstation.Server.Host.Jobs;
 using Tgstation.Server.Host.Utils;
 
-#nullable disable
-
 namespace Tgstation.Server.Host.Components.Watchdog
 {
 	/// <inheritdoc />
