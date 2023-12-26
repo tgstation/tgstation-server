1974d67bf1bf3bed12c90d74c0179ea84e72fd22
Jordan Dominion <Cyberboss@users.noreply.github.com>
2023-12-25 21:38:07 -0500

    Increase a deployment test timeout

--- tests/Tgstation.Server.Tests/Live/Instance/DeploymentTest.cs
+++ tests/Tgstation.Server.Tests/Live/Instance/DeploymentTest.cs
@@ -123,7 +123,7 @@ namespace Tgstation.Server.Tests.Live.Instance
 				var updatedDM = await dreamMakerClient.Update(new DreamMakerRequest
 				{
 					ProjectName = "tests/DMAPI/ApiFree/api_free",
-					ApiValidationPort = dmPort
+					ApiValidationPort = dmPort,
 				}, cancellationToken);
 				Assert.AreEqual(dmPort, updatedDM.ApiValidationPort);
 				Assert.AreEqual("tests/DMAPI/ApiFree/api_free", updatedDM.ProjectName);
@@ -153,7 +153,7 @@ namespace Tgstation.Server.Tests.Live.Instance
 
 			var deployJobTask = CompileAfterByondInstall();
 			var deployJob = await deployJobTask;
-			var deploymentJobWaitTask = WaitForJob(deployJob, 40, true, ErrorCode.DeploymentNeverValidated, cancellationToken);
+			var deploymentJobWaitTask = WaitForJob(deployJob, 120, true, ErrorCode.DeploymentNeverValidated, cancellationToken);
 
 			await CheckDreamDaemonPriority(deploymentJobWaitTask, cancellationToken);
 
