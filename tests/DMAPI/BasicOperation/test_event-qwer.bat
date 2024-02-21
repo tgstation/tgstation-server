bab8f93154ad394636511c1a7500af2a1136f50c
Jordan Dominion <Cyberboss@users.noreply.github.com>
2024-02-21 17:49:16 -0500

    Use a better sleep command on Windows
    Works in GitHub Actions

--- tests/DMAPI/BasicOperation/test_event-qwer.bat
+++ tests/DMAPI/BasicOperation/test_event-qwer.bat
@@ -1,7 +1,5 @@
 echo "Running test_event script"
-
-rem mingw has their own /usr/bin/timeout
-C:\Windows\system32\timeout.exe /t 5
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -Command "Start-Sleep -Seconds 5"
 cd %1
 cd tests\DMAPI\BasicOperation
 echo %2 > test_event_output.txt
