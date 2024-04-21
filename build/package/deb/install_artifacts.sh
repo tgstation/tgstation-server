40d69f3b0cb1c25dd07aba75a26c7d7db03d9c91
Jordan Dominion <Cyberboss@users.noreply.github.com>
2024-04-21 13:59:06 -0400

    Fix manpages installation

--- build/package/deb/install_artifacts.sh
+++ build/package/deb/install_artifacts.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/sh -e
 
 cd artifacts && for f in $(find * -type f); do install -D "$f" "$1/opt/tgstation-server/$f"; done
-cd ../build/package/deb/man && for f in $(find * -type f); do install -D "$f" "$1/usr/local/man/$f"; done
