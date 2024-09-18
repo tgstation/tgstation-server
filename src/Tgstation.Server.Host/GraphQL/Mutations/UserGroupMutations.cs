fa2fb000432fd1a9359d3e4d41a392fc859b4b6a
Jordan Dominion <Cyberboss@users.noreply.github.com>
2024-09-18 07:06:00 -0400

    Skeleton UserGroup mutations

---
+++ src/Tgstation.Server.Host/GraphQL/Mutations/UserGroupMutations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using HotChocolate;
+using HotChocolate.Types;
+using HotChocolate.Types.Relay;
+
+using Tgstation.Server.Host.Authority;
+using Tgstation.Server.Host.GraphQL.Mutations.Payloads;
+using Tgstation.Server.Host.GraphQL.Types;
+
+namespace Tgstation.Server.Host.GraphQL.Mutations
+{
+	/// <summary>
+	/// <see cref="IUserGroupAuthority"/> related <see cref="Mutation"/>s.
+	/// </summary>
+	[ExtendObjectType(typeof(Mutation))]
+	public sealed class UserGroupMutations
+	{
+		public ValueTask<UserGroup> CreateUserGroup(
+			string name,
+			PermissionSetInput permissionSet,
+			[Service] IUserGroupAuthority userGroupAuthority,
+			CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public ValueTask<UserGroup> UpdateUserGroup(
+			[ID(nameof(UserGroup))] long id,
+			string? newName,
+			PermissionSetInput newPermissionSet,
+			[Service] IUserGroupAuthority userGroupAuthority,
+			CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public ValueTask DeleteEmptyUserGroup(
+			[ID(nameof(UserGroup))] long id,
+			[Service] IUserGroupAuthority userGroupAuthority,
+			CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
