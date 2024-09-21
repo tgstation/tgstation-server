25a0e49e94e9649641b83d81160a2cfa6de9a695
Jordan Dominion <Cyberboss@users.noreply.github.com>
2024-09-21 17:30:22 -0400

    Document user group mutations

--- src/Tgstation.Server.Host/GraphQL/Mutations/UserGroupMutations.cs
+++ src/Tgstation.Server.Host/GraphQL/Mutations/UserGroupMutations.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -18,6 +18,14 @@ namespace Tgstation.Server.Host.GraphQL.Mutations
 	[ExtendObjectType(typeof(Mutation))]
 	public sealed class UserGroupMutations
 	{
+		/// <summary>
+		/// Creates a <see cref="UserGroup"/>.
+		/// </summary>
+		/// <param name="name">The <see cref="NamedEntity.Name"/> of the <see cref="UserGroup"/>.</param>
+		/// <param name="permissionSet">The initial permission set for the <see cref="UserGroup"/>.</param>
+		/// <param name="userGroupAuthority">The <see cref="IGraphQLAuthorityInvoker{TAuthority}"/> for the <see cref="IUserGroupAuthority"/>.</param>
+		/// <param name="cancellationToken">The <see cref="CancellationToken"/> for the operation.</param>
+		/// <returns>The created <see cref="UserGroup"/>.</returns>
 		public ValueTask<UserGroup> CreateUserGroup(
 			string name,
 			PermissionSetInput? permissionSet,
@@ -27,6 +35,15 @@ namespace Tgstation.Server.Host.GraphQL.Mutations
 			throw new NotImplementedException();
 		}
 
+		/// <summary>
+		/// Updates a <see cref="UserGroup"/>.
+		/// </summary>
+		/// <param name="id">The <see cref="Entity.Id"/> of the <see cref="UserGroup"/> to update.</param>
+		/// <param name="newName">Optional new <see cref="NamedEntity.Name"/> for the <see cref="UserGroup"/>.</param>
+		/// <param name="newPermissionSet">Optional new permission set for the <see cref="UserGroup"/>.</param>
+		/// <param name="userGroupAuthority">The <see cref="IGraphQLAuthorityInvoker{TAuthority}"/> for the <see cref="IUserGroupAuthority"/>.</param>
+		/// <param name="cancellationToken">The <see cref="CancellationToken"/> for the operation.</param>
+		/// <returns>The updated <see cref="UserGroup"/>.</returns>
 		public ValueTask<UserGroup> UpdateUserGroup(
 			[ID(nameof(UserGroup))] long id,
 			string? newName,
@@ -37,7 +54,14 @@ namespace Tgstation.Server.Host.GraphQL.Mutations
 			throw new NotImplementedException();
 		}
 
-		public ValueTask DeleteEmptyUserGroup(
+		/// <summary>
+		/// Deletes a <see cref="UserGroup"/>.
+		/// </summary>
+		/// <param name="id">The <see cref="Entity.Id"/> of the <see cref="UserGroup"/> to update.</param>
+		/// <param name="userGroupAuthority">The <see cref="IGraphQLAuthorityInvoker{TAuthority}"/> for the <see cref="IUserGroupAuthority"/>.</param>
+		/// <param name="cancellationToken">The <see cref="CancellationToken"/> for the operation.</param>
+		/// <returns>The <see cref="Query"/> root.</returns>
+		public ValueTask<Query> DeleteEmptyUserGroup(
 			[ID(nameof(UserGroup))] long id,
 			[Service] IUserGroupAuthority userGroupAuthority,
 			CancellationToken cancellationToken)
