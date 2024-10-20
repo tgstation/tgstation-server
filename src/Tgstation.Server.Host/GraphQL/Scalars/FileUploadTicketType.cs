770178c586a1840746334452d36aa7b0f62f7cd0
Jordan Dominion <Cyberboss@users.noreply.github.com>
2024-10-20 14:55:00 -0400

    Add `FileUploadTicket` scalar

---
+++ src/Tgstation.Server.Host/GraphQL/Scalars/FileUploadTicketType.cs
@@ -0,0 +1,17 @@
+﻿namespace Tgstation.Server.Host.GraphQL.Scalars
+{
+	/// <summary>
+	/// A <see cref="StringScalarType"/> for upload <see cref="Api.Models.Response.FileTicketResponse"/>s.
+	/// </summary>
+	public sealed class FileUploadTicketType : StringScalarType
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="FileUploadTicketType"/> class.
+		/// </summary>
+		public FileUploadTicketType()
+			: base("FileUploadTicket")
+		{
+			Description = "Represents a ticket that can be used with the file transfer service to upload a file";
+		}
+	}
+}
