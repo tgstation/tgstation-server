61a604b79fbd4b7747a23acf031dee9055a07c56
Drulikar <drathek@gmail.com>
2025-02-13 02:42:32 -0600

    Forgot one

--- src/Tgstation.Server.Host/Components/Deployment/Remote/GitHubRemoteDeploymentManager.cs
+++ src/Tgstation.Server.Host/Components/Deployment/Remote/GitHubRemoteDeploymentManager.cs
@@ -282,7 +282,7 @@ namespace Tgstation.Server.Host.Components.Deployment.Remote
 
 			try
 			{
-				string header = String.Format(CultureInfo.InvariantCulture, "<!-- test_merge_tgs_bot -->\n## Test merge deployment history:{0}{0}", Environment.NewLine);
+				string header = String.Format(CultureInfo.InvariantCulture, "<!-- test_merge_tgs_bot -->{0}## Test merge deployment history:{0}{0}", Environment.NewLine);
 				var existingComment = await gitHubService.GetExistingCommentOnIssue(remoteRepositoryOwner, remoteRepositoryName, header, testMergeNumber, cancellationToken);
 				if (existingComment != null)
 				{
