8b10619e2c5e9b728292f143ba8646e66b28b3ec
Jordan Dominion <Cyberboss@users.noreply.github.com>
2025-03-17 20:10:32 -0400

    Maybe fix release notes duplication

---
+++ tools/Tgstation.Server.ReleaseNotes/ChangelistEqualityComparer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+
+namespace Tgstation.Server.ReleaseNotes
+{
+	internal class ChangelistEqualityComparer : IEqualityComparer<Change>
+	{
+		public bool Equals(Change x, Change y)
+		{
+			if (x == y)
+				return true;
+
+			if (x == null)
+				return false;
+
+			if (y == null)
+				return false;
+
+			return JsonSerializer.Serialize(x) == JsonSerializer.Serialize(y);
+		}
+
+		public int GetHashCode([DisallowNull] Change obj)
+		{
+			return JsonSerializer.Serialize(obj).GetHashCode();
+		}
+	}
+}
