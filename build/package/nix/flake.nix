93c570bd2e3e2e6e200528cae6642ef4dba36229
Jordan Dominion <Cyberboss@users.noreply.github.com>
2025-07-26 11:03:59 -0400

    Try with nixpkgs

--- build/package/nix/flake.nix
+++ build/package/nix/flake.nix
@@ -3,12 +3,12 @@
 
     inputs = {};
 
-    outputs = { ... }: {
+    outputs = { nixpkgs, ... }: {
         nixosModules = {
             default = { ... }: {
                 imports = [ ./tgstation-server.nix ];
             };
         };
-        checks.x86_64-linux.package-build = pkgs.callPackage ./package.nix { };
+        checks.x86_64-linux.package-build = nixpkgs.legacyPackages.x86_64-linux.callPackage ./package.nix { };
     };
 }
