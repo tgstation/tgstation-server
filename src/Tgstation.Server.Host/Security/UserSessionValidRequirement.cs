9709aa64c212ac5b3ffbdd385d8cfa834f9c3981
Jordan Dominion <Cyberboss@users.noreply.github.com>
2025-07-27 18:47:51 -0400

    Minor syntax cleanup

--- src/Tgstation.Server.Host/Security/UserSessionValidRequirement.cs
+++ src/Tgstation.Server.Host/Security/UserSessionValidRequirement.cs
@@ -12,10 +12,10 @@ namespace Tgstation.Server.Host.Security
 		/// <summary>
 		/// The singleton instance of this class.
 		/// </summary>
-		public static IEnumerable<UserSessionValidRequirement> InstanceAsEnumerable { get; } = new List<UserSessionValidRequirement>
-		{
+		public static IEnumerable<UserSessionValidRequirement> InstanceAsEnumerable { get; } =
+		[
 			new(),
-		};
+		];
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="UserSessionValidRequirement"/> class.
