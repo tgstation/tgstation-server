be8d5e26b4416d183badefd516011ab87b3cf951
Jordan Dominion <Cyberboss@users.noreply.github.com>
2025-08-03 10:43:32 -0400

    Add deps generation script

---
+++ build/package/nix/GenerateDeps.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+function gen_dep() {
+    nuget-to-json ../../../src/$1/$1.csproj > deps/$1.json
+}
+
+gen_dep "Tgstation.Server.Common"
+gen_dep "Tgstation.Server.Shared"
+gen_dep "Tgstation.Server.Api"
+gen_dep "Tgstation.Server.Host.Utils.GitLab.GraphQL"
+gen_dep "Tgstation.Server.Host"
+gen_dep "Tgstation.Server.Host.Watchdog"
+gen_dep "Tgstation.Server.Host.Console"
